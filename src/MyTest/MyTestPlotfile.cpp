2b087819b28035867c833adf2fdda07219d61e93
Brandon Runnels <brunnels@uccs.edu>
2018-01-19 16:57:17 -0700

    figured out how to apply Dirichlet boundary conditions
    Former-commit-id: f9e4612fd3ac1e5cc94bedc233f2fb7bae894296

--- src/MyTest/MyTestPlotfile.cpp
+++ src/MyTest/MyTestPlotfile.cpp
@@ -8,7 +8,7 @@ void
 MyTest::writePlotfile () const
 {
     const int ncomp = (acoef.empty()) ? 5 : 7;
-    Vector<std::string> varname = {"solution", "solution2", "rhs", "exact_solution", "error"};
+    Vector<std::string> varname = {"solution", "boundary", "rhs", "exact_solution", "error"};
     if (!acoef.empty()) {
         varname.emplace_back("acoef");
         varname.emplace_back("bcoef");
@@ -21,7 +21,7 @@ MyTest::writePlotfile () const
     {
         plotmf[ilev].define(grids[ilev], dmap[ilev], ncomp, 0);
         MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 0, 1, 0);
-        MultiFab::Copy(plotmf[ilev], solution      [ilev], 1, 1, 1, 0);
+        MultiFab::Copy(plotmf[ilev], bcdata        [ilev], 0, 1, 1, 0);
         MultiFab::Copy(plotmf[ilev], rhs           [ilev], 0, 2, 1, 0);
         MultiFab::Copy(plotmf[ilev], exact_solution[ilev], 0, 3, 1, 0);
         MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 4, 1, 0);
