6788c1e76207e0ab07f18adbe3b759793da229ff
Brandon Runnels <brunnels@uccs.edu>
2018-01-19 15:17:35 -0700

    minor incremental change

--- src/MyTest/MyTestPlotfile.cpp
+++ src/MyTest/MyTestPlotfile.cpp
@@ -7,8 +7,8 @@ using namespace amrex;
 void
 MyTest::writePlotfile () const
 {
-    const int ncomp = (acoef.empty()) ? 4 : 6;
-    Vector<std::string> varname = {"solution", "rhs", "exact_solution", "error"};
+    const int ncomp = (acoef.empty()) ? 5 : 7;
+    Vector<std::string> varname = {"solution", "solution2", "rhs", "exact_solution", "error"};
     if (!acoef.empty()) {
         varname.emplace_back("acoef");
         varname.emplace_back("bcoef");
@@ -21,13 +21,14 @@ MyTest::writePlotfile () const
     {
         plotmf[ilev].define(grids[ilev], dmap[ilev], ncomp, 0);
         MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 0, 1, 0);
-        MultiFab::Copy(plotmf[ilev], rhs           [ilev], 0, 1, 1, 0);
-        MultiFab::Copy(plotmf[ilev], exact_solution[ilev], 0, 2, 1, 0);
-        MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 3, 1, 0);
-        MultiFab::Subtract(plotmf[ilev], plotmf[ilev], 2, 3, 1, 0); // error = soln - exact
+        MultiFab::Copy(plotmf[ilev], solution      [ilev], 1, 1, 1, 0);
+        MultiFab::Copy(plotmf[ilev], rhs           [ilev], 0, 2, 1, 0);
+        MultiFab::Copy(plotmf[ilev], exact_solution[ilev], 0, 3, 1, 0);
+        MultiFab::Copy(plotmf[ilev], solution      [ilev], 0, 4, 1, 0);
+        MultiFab::Subtract(plotmf[ilev], plotmf[ilev], 3, 4, 1, 0); // error = soln - exact
         if (!acoef.empty()) {
-            MultiFab::Copy(plotmf[ilev], acoef[ilev], 0, 4, 1, 0);
-            MultiFab::Copy(plotmf[ilev], bcoef[ilev], 0, 5, 1, 0);
+            MultiFab::Copy(plotmf[ilev], acoef[ilev], 0, 5, 1, 0);
+            MultiFab::Copy(plotmf[ilev], bcoef[ilev], 0, 6, 1, 0);
         }
     }
 
