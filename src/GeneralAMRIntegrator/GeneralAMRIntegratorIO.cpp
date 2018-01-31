695ac3c578d43c0c247884de28470a185b425ea9
Brandon Runnels <brunnels@uccs.edu>
2018-01-31 10:53:33 -0700

    initial implementation working
    Former-commit-id: b8e69dbb098157a7dacf4871d9bb09904b0b821e

--- src/GeneralAMRIntegrator/GeneralAMRIntegratorIO.cpp
+++ src/GeneralAMRIntegrator/GeneralAMRIntegratorIO.cpp
@@ -27,6 +27,7 @@ Array<const MultiFab*>
 GeneralAMRIntegrator::PlotFileMF (int fab) const
 {
   Array<const MultiFab*> r;
+  // TODO
   for (int i = 0; i <= finest_level; ++i) {
     //r.push_back(phi_new[fab][i].get());
   }
@@ -36,22 +37,65 @@ GeneralAMRIntegrator::PlotFileMF (int fab) const
 Array<std::string>
 GeneralAMRIntegrator::PlotFileVarNames () const
 {
-  Array<std::string> names;
+  //Array<std::string> names;
+  // TODO
   // for (int n = 0; n < number_of_grains; n++)
   //   names.push_back(amrex::Concatenate("phi",n));
-  names.push_back("phi_sum");
-  names.push_back("boundaries");
-  return names;
+  // names.push_back("phi_sum");
+  // names.push_back("boundaries");
+  //return names;
 }
 
 void
 GeneralAMRIntegrator::WritePlotFile () const
 {
+  // TODO
+  // const std::vector<std::string>& plotfilename = PlotFileName(istep[0]);
+  // const auto& mf = PlotFileMF(0);
+
+ 
+    // /**
+    // * \brief Copy from src to dst including nghost ghost cells.
+    // * The two MultiFabs MUST have the same underlying BoxArray.
+    // * The copy is local.  The parallel copy function is in FabArray.H
+    // */
+
+
+
+
+  const int nlevels = finest_level+1;
+
+  int total_components = 0;
+  for (int i = 0; i < number_of_fabs; i++)
+    total_components += (*fab_array[i])[0]->nComp();
+
+  Vector<MultiFab> plotmf(nlevels);
+  
+  for (int ilev = 0; ilev < nlevels; ++ilev)
+    {
+      plotmf[ilev].define(grids[ilev], dmap[ilev], total_components, 0);
+
+      int n = 0;
+      for (int i = 0; i < number_of_fabs; i++)
+	{
+	  // REFERENCE
+	  // static void Copy (MultiFab&       dst,
+	  //                   const MultiFab& src,
+	  //                   int             srccomp,
+	  //                   int             dstcomp,
+	  //                   int             numcomp,
+	  //                   int             nghost);
+
+	  MultiFab::Copy(plotmf[ilev], *(*fab_array[i])[ilev], 0, n, (*fab_array[i])[ilev]->nComp(), 0);
+	  n += (*fab_array[i])[ilev]->nComp();
+	}
+    }
+
   const std::vector<std::string>& plotfilename = PlotFileName(istep[0]);
-  const auto& mf = PlotFileMF(0);
-  const auto& varnames = PlotFileVarNames();
-  amrex::WriteMultiLevelPlotfile(plotfilename[0]+plotfilename[1], finest_level+1, mf, varnames,
-				 Geom(), t_new[0], istep, refRatio());
+  
+  WriteMultiLevelPlotfile(plotfilename[0]+plotfilename[1], nlevels, amrex::GetVecOfConstPtrs(plotmf), name_array,
+			  Geom(), t_new[0],istep, refRatio());
+
 
   if (ParallelDescriptor::IOProcessor())
     {
@@ -59,7 +103,7 @@ GeneralAMRIntegrator::WritePlotFile () const
       if (istep[0]==0) outfile.open(plot_file+"/output.visit",std::ios_base::out);
       else outfile.open(plot_file+"/output.visit",std::ios_base::app);
       outfile << plotfilename[1] + "/Header" << std::endl;
-}
+    }
 }
 
 void
