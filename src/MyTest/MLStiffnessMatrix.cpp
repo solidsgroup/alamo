23df9df9b2fd63fd279c304aa3c840d5a77e3740
Brandon Runnels <brunnels@uccs.edu>
2018-02-23 17:48:37 -0700

    almost done with fortran translation of fsmooth
    Former-commit-id: ea4ba5e3b0620352dde4b68c7fcdbca3a469b2dc

--- src/MyTest/MLStiffnessMatrix.cpp
+++ src/MyTest/MLStiffnessMatrix.cpp
@@ -1,6 +1,5 @@
 #include <AMReX_MultiFabUtil.H>
 #include "MLStiffnessMatrix.H"
-#include "AMReX_MLABecLap_F.H"
 #include "AMReX_ABec_F.H"
 
 namespace amrex {
@@ -19,7 +18,7 @@ MLStiffnessMatrix::define (const Vector<Geometry>& a_geom,
 			   const Vector<DistributionMapping>& a_dmap,
 			   const LPInfo& a_info)
 {
-  MLLinOp::define(a_geom, a_grids, a_dmap, a_info);
+  MLCellLinOp::define(a_geom, a_grids, a_dmap, a_info);
 
   m_a_coeffs.resize(m_num_amr_levels);
   m_b_coeffs.resize(m_num_amr_levels);
@@ -152,17 +151,17 @@ MLStiffnessMatrix::averageDownCoeffsToCoarseAmrLevel (int flev)
 void
 MLStiffnessMatrix::applyMetricTermsCoeffs ()
 {
-  for (int alev = 0; alev < m_num_amr_levels; ++alev)
+#if (AMREX_SPACEDIM != 3)
+    for (int alev = 0; alev < m_num_amr_levels; ++alev)
     {
-      for (int mglev = 0; mglev < m_num_mg_levels[alev]; ++mglev)
+        const int mglev = 0;
+        applyMetricTerm(alev, mglev, m_a_coeffs[alev][mglev]);
+        for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
         {
-	  applyMetricTerm(alev, mglev, m_a_coeffs[alev][mglev]);
-	  for (int idim = 0; idim < AMREX_SPACEDIM; ++idim)
-            {
-	      applyMetricTerm(alev, mglev, m_b_coeffs[alev][mglev][idim]);
-            }
+            applyMetricTerm(alev, mglev, m_b_coeffs[alev][mglev][idim]);
         }
     }
+#endif
 }
 
 void
@@ -170,17 +169,11 @@ MLStiffnessMatrix::prepareForSolve ()
 {
   BL_PROFILE("MLStiffnessMatrix::prepareForSolve()");
 
-  MLLinOp::prepareForSolve();
+  MLCellLinOp::prepareForSolve();
 
   applyMetricTermsCoeffs();
 
   averageDownCoeffs();
-
-  m_Anorm.resize(m_num_amr_levels);
-  for (int alev = 0; alev < m_num_amr_levels; ++alev)
-    {
-      m_Anorm[alev].assign(m_num_mg_levels[alev], -1.0);
-    }
 
   m_is_singular.clear();
   m_is_singular.resize(m_num_amr_levels, false);
@@ -252,6 +245,7 @@ MLStiffnessMatrix::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab
   AMREX_D_TERM(const MultiFab& bxcoef = m_b_coeffs[amrlev][mglev][0];,
 	       const MultiFab& bycoef = m_b_coeffs[amrlev][mglev][1];,
 	       const MultiFab& bzcoef = m_b_coeffs[amrlev][mglev][2];);
+
   const auto& undrrelxr = m_undrrelxr[amrlev][mglev];
   const auto& maskvals  = m_maskvals [amrlev][mglev];
 
@@ -293,6 +287,7 @@ MLStiffnessMatrix::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab
       const FArrayBox& f2fab = f2[mfi];
       const FArrayBox& f3fab = f3[mfi];
 
+      /// \note GSRB = Gauss-Seidel Red Black
       FORT_GSRB(solnfab.dataPtr(), ARLIM(solnfab.loVect()),ARLIM(solnfab.hiVect()),
 		rhsfab.dataPtr(), ARLIM(rhsfab.loVect()), ARLIM(rhsfab.hiVect()),
 		&m_a_scalar, &m_b_scalar,
@@ -310,6 +305,167 @@ MLStiffnessMatrix::Fsmooth (int amrlev, int mglev, MultiFab& sol, const MultiFab
 		tbx.loVect(), tbx.hiVect(), vbx.loVect(), vbx.hiVect(),
 		&nc, h, &redblack);
 
+      amrex::Real alpha = m_a_scalar, beta = m_b_scalar;
+
+      
+      int lo[2] = {tbx.loVect()[0], tbx.loVect()[1]};
+      int hi[2] = {tbx.hiVect()[0], tbx.hiVect()[1]};
+      int blo[2] = {vbx.loVect()[0], vbx.loVect()[1]};
+      int bhi[2] = {vbx.hiVect()[0], vbx.hiVect()[1]};
+
+      int do_line;
+      int ilen,jlen;
+
+      static int LSDIM = 127;
+      amrex::Real a_ls[LSDIM];// REAL_T a_ls(0:LSDIM)
+      amrex::Real b_ls[LSDIM];// REAL_T b_ls(0:LSDIM)
+      amrex::Real c_ls[LSDIM];// REAL_T c_ls(0:LSDIM)
+      amrex::Real r_ls[LSDIM];// REAL_T r_ls(0:LSDIM)
+      amrex::Real u_ls[LSDIM];// REAL_T u_ls(0:LSDIM)
+
+
+
+
+      if (h[1] > 1.5*h[0]) {        // if (h(2). gt. 1.5D0*h(1)) then 
+	  do_line = 1;              // do_line = 1
+	  ilen = hi[0] - lo[0] + 1; // hi[0]-lo[0]+1;
+	  if (ilen > LSDIM)         // if (ilen .gt. LSDIM) then
+	    amrex::Abort("Too big for Line Solve in GSRB: ilen="+ilen);  // print *,'TOO BIG FOR LINE SOLVE IN GSRB: ilen = ',ilen
+                                                                         // call bl_error("stop")
+	                                                                 // end if
+      }
+      else if (h[0] > 1.5*h[1]) {   // else if (h(1) .gt. 1.5D0*h(2)) then
+	  do_line = 2;              // do_line = 2
+	  jlen = hi[1] - lo[1] + 1; // jlen = hi(2)-lo(2)+1
+	  if (jlen > LSDIM)         // if (jlen .gt. LSDIM) then
+	    amrex::Abort("Too big for Line Solve in GSRB: ilen="+ilen);  // print *,'TOO BIG FOR LINE SOLVE IN GSRB: jlen = ',jlen
+                                                                         // call bl_error("stop")
+                                                                         // end if
+      }
+      else           //  else 
+	do_line = 0; // do_line = 0
+                     // end if
+
+      amrex::Real dhx, dhy;
+
+      dhx = beta/h[0]/h[0]; // dhx = beta/h(1)**2
+      dhy = beta/h[1]/h[1]; // dhy = beta/h(2)**2
+
+
+
+      for (int n = 0; n < nc; n++) // do n = 1, nc
+	if (do_line == 0)  {    //if (do_line .eq. 0) then
+	  for (int j = lo[1]; j <= hi[1]; j++){ //do j = lo(2), hi(2)
+	    int ioff = (lo[0] + j + redblack)%2; // ioff = MOD(lo(1) + j + redblack, 2)
+	    for (int i = lo[0]+ioff; i <= hi[0]; i += 2) {//do i = lo(1) + ioff,hi(1),2 
+	      amrex::Real cf0 = (i==blo[0])&&(m0(amrex::IntVect(blo[0]-1,j))>0) ? f0fab(amrex::IntVect(blo[0],j)) : 0.0; // cf0 = merge(f0(blo(1),j), 0.0D0,  (i .eq. blo(1)) .and. (m0(blo(1)-1,j).gt.0))
+	      amrex::Real cf1 = (j==blo[1])&&(m1(amrex::IntVect(i,blo[1]-1))>0) ? f1fab(amrex::IntVect(i,blo[1])) : 0.0; // cf1 = merge(f1(i,blo(2)), 0.0D0,  (j .eq. blo(2)) .and. (m1(i,blo(2)-1).gt.0))
+	      amrex::Real cf2 = (i==bhi[0])&&(m2(amrex::IntVect(bhi[0]+1,j))>0) ? f2fab(amrex::IntVect(bhi[0],j)) : 0.0; // cf2 = merge(f2(bhi(1),j), 0.0D0, (i .eq. bhi(1)) .and. (m2(bhi(1)+1,j).gt.0))
+	      amrex::Real cf3 = (j==bhi[1])&&(m3(amrex::IntVect(i,bhi[1]+1))>0) ? f3fab(amrex::IntVect(i,bhi[1])) : 0.0; // cf3 = merge(f3(i,bhi(2)), 0.0D0, (j .eq. bhi(2)) .and. (m3(i,bhi(2)+1).gt.0))
+     
+	      amrex::Real delta =
+		dhx * (bxfab(amrex::IntVect(i,j))*cf0 + bxfab(amrex::IntVect(i+1,j))*cf2) +  // delta = dhx*(bX(i,j)*cf0 + bX(i+1,j)*cf2)  +  dhy*(bY(i,j)*cf1 + bY(i,j+1)*cf3)
+		dhy * (byfab(amrex::IntVect(i,j))*cf1 + byfab(amrex::IntVect(i,j+1))*cf3);
+
+	      amrex::Real gamma =   // gamma = alpha*a(i,j) +   dhx*( bX(i,j) + bX(i+1,j) ) +   dhy*( bY(i,j) + bY(i,j+1) )
+		alpha*afab(amrex::IntVect(i,j)) +
+		dhx*(bxfab(amrex::IntVect(i,j)) + bxfab(amrex::IntVect(i+1,j))) +
+		dhy*(byfab(amrex::IntVect(i,j)) + byfab(amrex::IntVect(i,j+1)));
+
+	      amrex::Real rho =  //rho = dhx*(bX(i,j)*phi(i-1,j,n) + bX(i+1,j)*phi(i+1,j,n))+dhy*(bY(i,j)*phi(i,j-1,n) + bY(i,j+1)*phi(i,j+1,n))
+		dhx*(bxfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i-1,j),n) + bxfab(amrex::IntVect(i+1,j))*solnfab(amrex::IntVect(i+1,j),n)) +
+		dhy*(byfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i,j-1),n) + byfab(amrex::IntVect(i,j+1))*solnfab(amrex::IntVect(i,j+1),n));
+
+	      solnfab(amrex::IntVect(i,j),n) = (rhsfab(amrex::IntVect(i,j),n) + rho - rhsfab(amrex::IntVect(i,j),n)*delta) / (gamma-delta); //phi(i,j,n) = (rhs(i,j,n) + rho - phi(i,j,n)*delta)/(gamma - delta)
+	      
+	    }   //  end do
+	  }  // end do
+	}
+	else if (do_line == 2) {  // else if (do_line .eq. 2) then
+	  int ioff = (lo[0]+redblack)%2; //          ioff = MOD(lo(1) + redblack, 2)
+	  for (int i = lo[0]+ioff; i <= hi[0]; i+=2) {//          do i = lo(1) + ioff,hi(1),2
+	    for (int j = lo[1]; j<=hi[1]; j++) {//              do j = lo(2), hi(2)
+	      amrex::Real cf0 = (i==blo[0])&&(m0(amrex::IntVect(blo[0]-1,j))>0) ? f0fab(amrex::IntVect(blo[0],j)) : 0.0;//cf0 = merge(f0(blo(1),j), 0.0D0, (i .eq. blo(1)) .and. (m0(blo(1)-1,j).gt.0))
+	      amrex::Real cf1 = (j==blo[1])&&(m1(amrex::IntVect(i,blo[1]-1))>0) ? f1fab(amrex::IntVect(i,blo[1])) : 0.0;//cf1 = merge(f1(i,blo(2)), 0.0D0, (j .eq. blo(2)) .and. (m1(i,blo(2)-1).gt.0))
+	      amrex::Real cf2 = (i==bhi[0])&&(m2(amrex::IntVect(bhi[0]+1,j))>0) ? f2fab(amrex::IntVect(bhi[0],j)) : 0.0;//cf2 = merge(f2(bhi(1),j), 0.0D0, (i .eq. bhi(1)) .and. (m2(bhi(1)+1,j).gt.0))
+	      amrex::Real cf3 = (j==bhi[1])&&(m3(amrex::IntVect(i,bhi[1]+1))>0) ? f3fab(amrex::IntVect(i,bhi[1])) : 0.0;//cf3 = merge(f3(i,bhi(2)), 0.0D0, (j .eq. bhi(2)) .and. (m3(i,bhi(2)+1).gt.0))
+
+	      amrex::Real delta =
+		dhx * (bxfab(amrex::IntVect(i,j))*cf0 + bxfab(amrex::IntVect(i+1,j))*cf2) +  // delta = dhx*(bX(i,j)*cf0 + bX(i+1,j)*cf2)  +  dhy*(bY(i,j)*cf1 + bY(i,j+1)*cf3)
+		dhy * (byfab(amrex::IntVect(i,j))*cf1 + byfab(amrex::IntVect(i,j+1))*cf3);
+
+	      amrex::Real gamma =   // gamma = alpha*a(i,j) +   dhx*( bX(i,j) + bX(i+1,j) ) +   dhy*( bY(i,j) + bY(i,j+1) )
+		alpha*afab(amrex::IntVect(i,j)) +
+		dhx*(bxfab(amrex::IntVect(i,j)) + bxfab(amrex::IntVect(i+1,j))) +
+		dhy*(byfab(amrex::IntVect(i,j)) + byfab(amrex::IntVect(i,j+1)));
+
+	      amrex::Real rho_x =  // rho_x = dhx*(bX(i,j)*phi(i-1,j,n) + bX(i+1,j)*phi(i+1,j,n)=))
+		dhx*(bxfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i-1,j),n) + bxfab(amrex::IntVect(i+1,j))*solnfab(amrex::IntVect(i+1,j),n));
+		     
+	      
+	      a_ls[j-lo[1]] = -dhy*byfab(amrex::IntVect(i,j)); //  a_ls(j-lo(2)) = -dhy*bY(i,j)
+	      b_ls[j-lo[1]] = gamma-delta; // b_ls(j-lo(2)) = gamma - delta
+	      c_ls[j-lo[1]] = -dhy*byfab(amrex::IntVect(i,j+1)); // c_ls(j-lo(2)) = -dhy*bY(i,j+1)
+	      r_ls[j-lo[1]] = rhsfab(amrex::IntVect(i,j),n) + rho_x - solnfab(amrex::IntVect(i,j),n)*delta; // r_ls(j-lo(2)) = rhs(i,j,n) + rho_x - phi(i,j,n)*delta
+
+	      if (j==lo[1])      //  if (j .eq. lo(2)) 
+		r_ls[j-lo[1]] =  r_ls[j-lo[1]] + dhy*byfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i,j-1),n); //  r_ls(j-lo(2)) = r_ls(j-lo(2)) + dhy*bY(i,j)*phi(i,j-1,n)
+
+	      if (j==hi[1])  //if (j .eq. hi(2)) 
+		r_ls[j-lo[1]] = r_ls[j-lo[1]] + dhy*byfab(amrex::IntVect(i,j+1))*solnfab(amrex::IntVect(i,j+1),n); // r_ls(j-lo(2)) = r_ls(j-lo(2)) + dhy*bY(i,j+1)*phi(i,j+1,n)
+
+	    } //end do
+	    
+	    // TODO TODO TODO TODO TODO // call tridiag(a_ls,b_ls,c_ls,r_ls,u_ls,jlen)
+
+	    for (int j = lo[1]; j <= hi[1]; j++) {  // do j = lo(2), hi(2)
+	      solnfab(amrex::IntVect(i,j),n) = u_ls[j-lo[1]]; //  phi(i,j,n) = u_ls(j-lo(2))
+	    }// end do
+	  } // end do
+	}
+	else if (do_line == 1) {// else if (do_line .eq. 1) then
+	  int joff = (lo[1] + redblack)%2; // joff = MOD(lo(2) + redblack, 2)
+	  for (int j=lo[1]+joff; j<= hi[1]; j+= 2) { // do j = lo(2) + joff,hi(2),2
+	    for (int i=lo[0]; i<= hi[0]; i++) {// do i = lo(1), hi(1)
+	      amrex::Real cf0 = (i==blo[0])&&(m0(amrex::IntVect(blo[0]-1,j))>0) ? f0fab(amrex::IntVect(blo[0],j)) : 0.0;//cf0 = merge(f0(blo(1),j), 0.0D0, (i .eq. blo(1)) .and. (m0(blo(1)-1,j).gt.0))
+	      amrex::Real cf1 = (j==blo[1])&&(m1(amrex::IntVect(i,blo[1]-1))>0) ? f1fab(amrex::IntVect(i,blo[1])) : 0.0;//cf1 = merge(f1(i,blo(2)), 0.0D0, (j .eq. blo(2)) .and. (m1(i,blo(2)-1).gt.0))
+	      amrex::Real cf2 = (i==bhi[0])&&(m2(amrex::IntVect(bhi[0]+1,j))>0) ? f2fab(amrex::IntVect(bhi[0],j)) : 0.0;//cf2 = merge(f2(bhi(1),j), 0.0D0, (i .eq. bhi(1)) .and. (m2(bhi(1)+1,j).gt.0))
+	      amrex::Real cf3 = (j==bhi[1])&&(m3(amrex::IntVect(i,bhi[1]+1))>0) ? f3fab(amrex::IntVect(i,bhi[1])) : 0.0;//cf3 = merge(f3(i,bhi(2)), 0.0D0, (j .eq. bhi(2)) .and. (m3(i,bhi(2)+1).gt.0))
+
+	      amrex::Real delta =
+		dhx * (bxfab(amrex::IntVect(i,j))*cf0 + bxfab(amrex::IntVect(i+1,j))*cf2) +  // delta = dhx*(bX(i,j)*cf0 + bX(i+1,j)*cf2)  +  dhy*(bY(i,j)*cf1 + bY(i,j+1)*cf3)
+		dhy * (byfab(amrex::IntVect(i,j))*cf1 + byfab(amrex::IntVect(i,j+1))*cf3);
+
+	      amrex::Real gamma =   // gamma = alpha*a(i,j) +   dhx*( bX(i,j) + bX(i+1,j) ) +   dhy*( bY(i,j) + bY(i,j+1) )
+		alpha*afab(amrex::IntVect(i,j)) +
+		dhx*(bxfab(amrex::IntVect(i,j)) + bxfab(amrex::IntVect(i+1,j))) +
+		dhy*(byfab(amrex::IntVect(i,j)) + byfab(amrex::IntVect(i,j+1)));
+
+	      amrex::Real rho_y =   // rho_y = dhy*(bY(i,j)*phi(i,j-1,n) + bY(i,j+1)*phi(i,j+1,n))
+		dhy * (byfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i,j-1),n) + byfab(amrex::IntVect(i,j+1))*solnfab(amrex::IntVect(i,j+1),n));
+
+	      a_ls[i-lo[0]] = -dhx*bxfab(amrex::IntVect(i,j)); // a_ls(i-lo(1)) = -dhx*bX(i,j)
+	      b_ls[i-lo[0]] = gamma-delta; // b_ls(i-lo(1)) = gamma - delta
+	      c_ls[i-lo[0]] = -dhx*byfab(amrex::IntVect(i+1,j)); // c_ls(i-lo(1)) = -dhx*bX(i+1,j)
+	      r_ls[i-lo[0]] = rhsfab(amrex::IntVect(i,j),n) + rho_y - solnfab(amrex::IntVect(i,j),n)*delta; // r_ls(i-lo(1)) = rhs(i,j,n) + rho_y - phi(i,j,n)*delta
+
+	      if (i==lo[0]) // if (i .eq. lo(1)) 
+		r_ls[i-lo[0]] = r_ls[i-lo[0]] + dhx*bxfab(amrex::IntVect(i,j))*solnfab(amrex::IntVect(i-1,j),n); // r_ls(i-lo(1)) = r_ls(i-lo(1)) + dhx*bX(i,j)*phi(i-1,j,n)
+
+	      if (i==hi[0]) // if (i .eq. hi(1)) 
+		r_ls[i-lo[0]] = r_ls[i-lo[0]] + dhx*bxfab(amrex::IntVect(i+1,j))*solnfab(amrex::IntVect(i+1,j),n); //  r_ls(i-lo(1)) = r_ls(i-lo(1)) + dhx*bX(i+1,j)*phi(i+1,j,n)
+	    }//  end do
+
+	    // TODO TODO TODO TODO TODO // call tridiag(a_ls,b_ls,c_ls,r_ls,u_ls,ilen)
+	    for (int i=lo[0]; i<= hi[0]; i++) { // do i = lo(1), hi(1)
+	      solnfab(amrex::IntVect(i,j),n) = u_ls[i-lo[0]]; // phi(i,j,n) = u_ls(i-lo(1))
+	    } //  end do
+	  } // end do
+	}
+	else {//        else
+	  amrex::Abort("Bogus Do Line"); //          print *,'BOGUS DO_LINE '
+	                                 //          call bl_error("stop")
+	} //        end if
     }
 }
 
@@ -318,6 +474,7 @@ MLStiffnessMatrix::FFlux (int amrlev, const MFIter& mfi,
  			  const std::array<FArrayBox*,AMREX_SPACEDIM>& flux,
  			  const FArrayBox& sol, const int face_only) const
 {
+  std::cout << "in FFlux, face_only=" << face_only << std::endl;
   BL_PROFILE("MLStiffnessMatrix::FFlux()");
 
   const int mglev = 0;
@@ -334,6 +491,8 @@ MLStiffnessMatrix::FFlux (int amrlev, const MFIter& mfi,
   amrex::BaseFab<amrex::Real> &fxfab = *flux[0];
   amrex::BaseFab<amrex::Real> &fyfab = *flux[1];
   const amrex::BaseFab<amrex::Real> &solfab = sol;
+
+
 
   if (face_only)
     {
@@ -372,56 +531,6 @@ MLStiffnessMatrix::FFlux (int amrlev, const MFIter& mfi,
     }
 }
 
-Real
-MLStiffnessMatrix::Anorm (int amrlev, int mglev) const
-{
-  BL_PROFILE("MLStiffnessMatrix::Anorm()");
-
-  if (m_Anorm[amrlev][mglev] < 0.0)
-    {
-      const MultiFab& acoef = m_a_coeffs[amrlev][mglev];
-      AMREX_D_TERM(const MultiFab& bxcoef = m_b_coeffs[amrlev][mglev][0];,
-		   const MultiFab& bycoef = m_b_coeffs[amrlev][mglev][1];,
-		   const MultiFab& bzcoef = m_b_coeffs[amrlev][mglev][2];);
-      const Real* dx = m_geom[amrlev][mglev].CellSize();
-
-      const int nc = 1;
-      Real res = 0.0;
-
-#ifdef _OPENMP
-#pragma omp parallel reduction(max:res)
-#endif
-      {
-	for (MFIter mfi(acoef,true); mfi.isValid(); ++mfi)
-	  {
-	    Real tres;
-	    
-	    const Box&       tbx  = mfi.tilebox();
-	    const FArrayBox& afab = acoef[mfi];
-	    AMREX_D_TERM(const FArrayBox& bxfab = bxcoef[mfi];,
-			 const FArrayBox& byfab = bycoef[mfi];,
-			 const FArrayBox& bzfab = bzcoef[mfi];);
-
-	    FORT_NORMA(&tres,
-		       &m_a_scalar, &m_b_scalar,
-		       afab.dataPtr(),  ARLIM(afab.loVect()), ARLIM(afab.hiVect()),
-		       bxfab.dataPtr(), ARLIM(bxfab.loVect()), ARLIM(bxfab.hiVect()),
-		       byfab.dataPtr(), ARLIM(byfab.loVect()), ARLIM(byfab.hiVect()),
-		       tbx.loVect(), tbx.hiVect(), &nc, dx);
-                
-	    res = std::max(res, tres);
-	  }
-      }
-        
-      ParallelAllReduce::Max(res, Communicator(amrlev,mglev));
-      m_Anorm[amrlev][mglev] = res;
-    }
-
-  return m_Anorm[amrlev][mglev];
-}
-
-
-#if (BL_SPACEDIM == 2)
 amrex::Real
 MLStiffnessMatrix::K (amrex::IntVect m, amrex::IntVect n,int i, int k)
 {
@@ -496,8 +605,6 @@ MLStiffnessMatrix::K (amrex::IntVect m, amrex::IntVect n,int i, int k)
   return mu * (i==k ? 1. : 0.) * (DPhiDPhi[0][0] + DPhiDPhi[1][1]) + (mu + lambda) * DPhiDPhi[i][k];
 
 }
-#endif
-
 
 }
 
