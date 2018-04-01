45d89a3ab4d020a7cc31c07ee112e0a223a3b1bd
Vinamra Agrawal <vinagr@auburn.edu>
2018-04-01 17:05:57 -0500

    Changed the way derivative is computed at the boundary in FApply and FSmooth
    Former-commit-id: c9b74531a3435fa2c51dc20e3c33ea0c818f14a2

--- src/MyTest/MLPFStiffnessMatrix.cpp
+++ src/MyTest/MLPFStiffnessMatrix.cpp
@@ -99,7 +99,7 @@ MLPFStiffnessMatrix::prepareForSolve ()
 }
 
 /// \fn MLPFStiffnessMatrix::Fapply
-/// 
+///
 /// Numerically implement the operator
 ///
 /// \f[ f_i = \mathbb{C}_{ijkl} u_{k,jl}\f]
@@ -113,7 +113,7 @@ MLPFStiffnessMatrix::prepareForSolve ()
 ///
 /// \f[f_p = 2\mu\varepsilon_{pq,q} + \lambda\delta_{pq}\varepsilon_{kk,q}\f]
 ///
-/// where \f$\mu\f$ = \link #mu \endlink member variable and 
+/// where \f$\mu\f$ = \link #mu \endlink member variable and
 ///
 /// \f$\lambda\f$ = \link #lambda \endlink member variable.
 ///
@@ -135,7 +135,7 @@ MLPFStiffnessMatrix::Fapply (int amrlev, ///<[in] AMR Level
 
       const amrex::BaseFab<amrex::Real> &ufab  = u[mfi];
       amrex::BaseFab<amrex::Real>       &ffab  = f[mfi];
-      
+
 
       for (int m = bx.loVect()[0]; m<=bx.hiVect()[0]; m++)
 	for (int n = bx.loVect()[1]; n<=bx.hiVect()[1]; n++)
@@ -149,10 +149,10 @@ MLPFStiffnessMatrix::Fapply (int amrlev, ///<[in] AMR Level
 
 		    // C_{ijkl} u_{k,jl}
 
-		    ffab(amrex::IntVect(m,n),i) -= 
+		    ffab(amrex::IntVect(m,n),i) -=
 		      C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi)
-		      *(ufab(amrex::IntVect(m+1,n),k) - 2.0*ufab(amrex::IntVect(m,n),k) + ufab(amrex::IntVect(m-1,n),k))/dx[0]/dx[0] 
-		      + 
+		      *(ufab(amrex::IntVect(m+1,n),k) - 2.0*ufab(amrex::IntVect(m,n),k) + ufab(amrex::IntVect(m-1,n),k))/dx[0]/dx[0]
+		      +
 		      (C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) + C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))
 		      *(ufab(amrex::IntVect(m+1,n+1),k) + ufab(amrex::IntVect(m-1,n-1),k) - ufab(amrex::IntVect(m+1,n-1),k) - ufab(amrex::IntVect(m-1,n+1),k))/(2.0*dx[0])/(2.0*dx[1])
 		      +
@@ -160,16 +160,49 @@ MLPFStiffnessMatrix::Fapply (int amrlev, ///<[in] AMR Level
 		      *(ufab(amrex::IntVect(m,n+1),k) - 2.0*ufab(amrex::IntVect(m,n),k) + ufab(amrex::IntVect(m,n-1),k))/dx[1]/dx[1];
 
 		    // C_{ijkl,j} u_{k,l}
+				amrex::Real temp1, temp2, temp3, temp4;
+				if(m == bx.loVect()[0])
+				{
+					temp1 = (C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[0]);
+					temp3 = (C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[0]);
+				}
+				else if(m == bx.hiVect()[0])
+				{
+					temp1 = (C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(dx[0]);
+					temp3 = (C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(dx[0]);
+				}
+				else
+				{
+					temp1 = (C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]);
+					temp3 = (C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]);
+				}
+				if(n == bx.loVect()[1])
+				{
+					temp2 = (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[1]);
+					temp4 = (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[1]);
+				}
+				else if(n == bx.hiVect()[1])
+				{
+					temp2 = (C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(dx[1]);
+					temp4 = (C(i,1,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(dx[1]);
+				}
+				else
+				{
+					temp2 = (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1]);
+					temp4 = (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1]);
+				}
 
-		    ffab(amrex::IntVect(m,n),i) -= 
-		      ((C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
-		       (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
-		      ((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0]))
-		      +
-		      ((C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
-		       (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
-		      ((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
-		      
+				ffab(amrex::IntVect(m,n),i) -= (temp1 + temp2)*((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0])) +
+					(temp3 + temp4)*((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
+				//ffab(amrex::IntVect(m,n),i) -=
+		      //((C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
+		       //(C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
+		      //((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0]))
+		      //+
+		      //((C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
+		       //(C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
+		      //((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
+
 		  }
 	      }
 	  }
@@ -178,17 +211,17 @@ MLPFStiffnessMatrix::Fapply (int amrlev, ///<[in] AMR Level
 
 
 /// \fn MLPFStiffnessMatrix::Fsmooth
-/// 
+///
 /// Perform one half Gauss-Seidel iteration corresponding to the operator specified
 /// in MLPFStiffnessMatrix::Fapply.
 /// The variable redblack corresponds to whether to smooth "red" nodes or "black"
 /// nodes, where red and black nodes are distributed in a checkerboard pattern.
-/// 
+///
 /// \todo Extend to 3D
 ///
 void
 MLPFStiffnessMatrix::Fsmooth (int amrlev,          ///<[in] AMR level
-			      int mglev,           ///<[in] 
+			      int mglev,           ///<[in]
 			      MultiFab& u,       ///<[inout] Solution (displacement field)
 			      const MultiFab& rhs, ///<[in] Body force vectors (rhs=right hand side)
 			      int redblack         ///<[in] Variable to determine whether to smooth even or odd modes
@@ -217,14 +250,14 @@ MLPFStiffnessMatrix::Fsmooth (int amrlev,          ///<[in] AMR level
 	      for (int i=0; i<AMREX_SPACEDIM; i++)
 		{
 		  amrex::Real rho = 0.0, aa = 0.0;
-		      
+
 		  for (int k=0; k<AMREX_SPACEDIM; k++)
 		    {
 
-		      rho -= 
+		      rho -=
 			C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi)
 			*(ufab(amrex::IntVect(m+1,n),k) - (i==k ? 0.0 : 2.0*ufab(amrex::IntVect(m,n),k)) + ufab(amrex::IntVect(m-1,n),k))/dx[0]/dx[0]
-			+ 
+			+
 			(C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) + C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))
 			*(ufab(amrex::IntVect(m+1,n+1),k) + ufab(amrex::IntVect(m-1,n-1),k) - ufab(amrex::IntVect(m+1,n-1),k) - ufab(amrex::IntVect(m-1,n+1),k))/(2.0*dx[0])/(2.0*dx[1])
 			+
@@ -232,16 +265,49 @@ MLPFStiffnessMatrix::Fsmooth (int amrlev,          ///<[in] AMR level
 			*(ufab(amrex::IntVect(m,n+1),k) - (i==k ? 0.0 : 2.0*ufab(amrex::IntVect(m,n),k)) + ufab(amrex::IntVect(m,n-1),k))/dx[1]/dx[1];
 
 		      // C_{ijkl,j} u_{k,l}
+					amrex::Real temp1, temp2, temp3, temp4;
+					if(m == tbx.loVect()[0])
+					{
+						temp1 = (C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[0]);
+						temp3 = (C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[0]);
+					}
+					else if(m == tbx.hiVect()[0])
+					{
+						temp1 = (C(i,0,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(dx[0]);
+						temp3 = (C(i,0,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(dx[0]);
+					}
+					else
+					{
+						temp1 = (C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]);
+						temp3 = (C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]);
+					}
+					if(n == tbx.loVect()[1])
+					{
+						temp2 = (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[1]);
+						temp4 = (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi))/(dx[1]);
+					}
+					else if(n == tbx.hiVect()[1])
+					{
+						temp2 = (C(i,1,k,0,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(dx[1]);
+						temp4 = (C(i,1,k,1,amrex::IntVect(m,n),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(dx[1]);
+					}
+					else
+					{
+						temp2 = (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1]);
+						temp4 = (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1]);
+					}
 
-		      rho -= 
-		        ((C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
-		         (C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
-		        ((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0]))
-		        +
-		        ((C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
-		         (C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
-		        ((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
-		      
+					rho -= (temp1+temp2)*((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0])) +
+						(temp3+temp4)*((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
+		      //rho -=
+		        //((C(i,0,k,0,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,0,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
+		         //(C(i,1,k,0,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,0,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
+		        //((ufab(amrex::IntVect(m+1,n),k) - ufab(amrex::IntVect(m-1,n),k))/(2.0*dx[0]))
+		        //+
+		        //((C(i,0,k,1,amrex::IntVect(m+1,n),amrlev,mglev,mfi) - C(i,0,k,1,amrex::IntVect(m-1,n),amrlev,mglev,mfi))/(2.0*dx[0]) +
+		         //(C(i,1,k,1,amrex::IntVect(m,n+1),amrlev,mglev,mfi) - C(i,1,k,1,amrex::IntVect(m,n-1),amrlev,mglev,mfi))/(2.0*dx[1])) *
+		        //((ufab(amrex::IntVect(m,n+1),k) - ufab(amrex::IntVect(m,n-1),k))/(2.0*dx[1]));
+
 
 
 		      if (rho != rho){
@@ -279,13 +345,13 @@ MLPFStiffnessMatrix::Fsmooth (int amrlev,          ///<[in] AMR level
 
 		    }
 
-		  aa -= 
+		  aa -=
 		    -2.0*C(i,0,i,0,amrex::IntVect(m,n),amrlev,mglev,mfi)/dx[0]/dx[0]
 		    -2.0*C(i,1,i,1,amrex::IntVect(m,n),amrlev,mglev,mfi)/dx[1]/dx[1];
 
 		  //if (rho != rho) amrex::Abort("nans detected");
 		  //if (rho != rho) std::cout << "nans detetected" << std::endl;
-		  
+
 		  ufab(amrex::IntVect(m,n),i) = (rhsfab(amrex::IntVect(m,n),i) - rho) / aa;
 		}
 	    }
@@ -295,12 +361,12 @@ MLPFStiffnessMatrix::Fsmooth (int amrlev,          ///<[in] AMR level
 }
 
 /// \fn MLPFStiffnessMatrix::FFlux
-/// 
+///
 /// Compute the "flux" corresponding to the operator in MLPFStiffnessMatrix::Fapply.
 /// Because the operator is self-adjoint and positive-definite, the flux is not
 /// required for adequate convergence (?)
 /// Therefore, the fluxes are simply set to zero and returned.
-/// 
+///
 /// \todo Extend to 3D
 ///
 void
@@ -338,9 +404,14 @@ MLPFStiffnessMatrix::C(const int i, const int j, const int k, const int l,
   const amrex::BaseFab<amrex::Real> &etafab = m_a_coeffs[amrlev][mglev][mfi];
   mu = (mu1*etafab(loc,0) + mu2*etafab(loc,1)) / (etafab(loc,0) + etafab(loc,1));
   lambda = (lambda1*etafab(loc,0) + lambda2*etafab(loc,1)) / (etafab(loc,0) + etafab(loc,1));
- 
+
   // TODO: This is a hack, needs to be fixed.
-  if (mu != mu) mu = 0.5*(mu1+mu2);
+  if (mu != mu)
+	{
+		std::cout << "Nans detected i = " << i << " j = " << j << " k = " << k << " l = " << l << std:: endl;
+		std::cout << "etafab = " << etafab(loc,0) << " , " << etafab(loc,1) << std::endl;
+		mu = 0.5*(mu1+mu2);
+	}
   if (lambda != lambda) lambda = 0.5*(lambda1+lambda2);
 
   //std::cout << "(" << mu << ","<<lambda << ") ";
@@ -360,5 +431,3 @@ MLPFStiffnessMatrix::C(const int i, const int j, const int k, const int l,
 
   return ret;
 };
-
-
