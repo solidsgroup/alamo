e8ac7894615ef80719f2a2fed822557d3b266cab
p8l <cpool@outlook.com>
2018-04-29 00:07:13 -0600

    changed fem to CubicRotation and fixed bugs in CubicRotation
    Former-commit-id: 83494574d94e01aab1f82cfa59a2ad1b7f35d48f

--- src/Operator/Elastic/CubicRotation/CubicRotation.cpp
+++ src/Operator/Elastic/CubicRotation/CubicRotation.cpp
@@ -2,22 +2,21 @@
 #include <AMReX_REAL.H>
 #include <AMReX_MLCGSolver.H>
 #include <AMReX_ArrayLim.H>
+//#include <iostream>
 
 #include "eigen3/Eigen/Core" //not sure if this import is necessary
 
 #include "Operator/Elastic/CubicRotation/CubicRotation.H"
-//#include "Operator/Elastic/Cubic/Cubic.H"  //allows use of Cijkl internally
 
-Operator::Elastic::CubicRotation::CubicRotation(Eigen::Matrix<amrex::Real, AMREX_SPACEDIM, AMREX_SPACEDIM> R,
-						amrex::Real C11in, amrex::Real C12in, amrex::Real C44in)
+Operator::Elastic::CubicRotation::CubicRotation(Eigen::Matrix<amrex::Real, AMREX_SPACEDIM, AMREX_SPACEDIM> R)
 {
-  // Default values hard coded for now
-  E1 = 1.0; nu1 = 0.25; mu1 = 2.0;
-  E2 = 1.0; nu2 = 0.25; mu2 = 2.0;
-  C11 = C11in;
-  C12 = C12in;
-  C44 = C44in;
-  
+  E = 1.0; nu = 0.25; mu = 2.0;
+
+  C11 = E*(1-nu)/(1-nu-2.0*nu*nu);
+  C12 = E*nu/(1-nu-2.0*nu*nu);
+  C44 = mu;
+  //std::cout << "this is C11: " << C11 << "  this is C12: " << C12 << "  this is C44: " << C44 << "\n";
+  //std::cout << "this is R: " << R(0,0) << " " << R(0,1) << " " << R(1,0) << " " << R(1,1) << "\n";
   amrex::Real C[AMREX_SPACEDIM][AMREX_SPACEDIM][AMREX_SPACEDIM][AMREX_SPACEDIM];  //may create naming conflict w/ C()
   
   for(int i = 0; i < AMREX_SPACEDIM; i++) {
@@ -38,30 +37,34 @@ Operator::Elastic::CubicRotation::CubicRotation(Eigen::Matrix<amrex::Real, AMREX
     }
   }
   
-  //amrex::Real Ctmp[AMREX_SPACEDIM][AMREX_SPACEDIM][AMREX_SPACEDIM][AMREX_SPACEDIM];
   amrex::Real SumStore;
   
   for(int p = 0; p < AMREX_SPACEDIM; p++) {
     for(int q = 0; q < AMREX_SPACEDIM; q++) {
       for(int s = 0; s < AMREX_SPACEDIM; s++) {
         for(int t = 0; t < AMREX_SPACEDIM; t++) {
-	  SumStore = 0.0; //replace with C that has stuff added to it
+	  Ctmp[p][q][s][t] = 0.0;
 	  for(int i = 0; i < AMREX_SPACEDIM; i++) {
 	    for(int j = 0; j < AMREX_SPACEDIM; j++) {
 	      for(int k = 0; k < AMREX_SPACEDIM; k++) {
 		for(int l = 0; l < AMREX_SPACEDIM; l++) {
-		  SumStore += R(p,i)*R(s,k)*C[i][j][k][l]*R(q,i)*R(t,l);
+		  Ctmp[p][q][s][t] += R(p,i)*R(s,k)*C[i][j][k][l]*R(q,j)*R(t,l);
+		  //std::cout << "this is Ctmp: " << Ctmp[p][q][s][t] << " with " << p << q << s << t;
 		}
 	      }
 	    }
 	  }
-	  Ctmp[p][q][s][t] = SumStore;
+	  //std::cout << "this is Ctmp: " << Ctmp[p][q][s][t] << " with " << p << q << s << t << "\n";
+	  //std::cout << "this is C: " << C[p][q][s][t] << " with " << p << q << s << t << "\n";
 	}
       }
     }
   }
 
 }
+
+
+  
 
 void
 Operator::Elastic::CubicRotation::SetEta(amrex::Array<std::unique_ptr<amrex::MultiFab> > &eta, GeneralAMRIntegratorPhysBC &eta_bc)
