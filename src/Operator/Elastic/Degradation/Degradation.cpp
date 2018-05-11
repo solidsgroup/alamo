87f462c8fc7b7033d6e33b56043a6f0b5315882b
Vinamra Agrawal <vinagr@auburn.edu>
2018-05-11 14:03:08 -0500

    Fixed some compilation issues by hard coding a few things. Will fix this later

--- src/Operator/Elastic/Degradation/Degradation.cpp
+++ src/Operator/Elastic/Degradation/Degradation.cpp
@@ -5,26 +5,26 @@
 
 #include "Degradation.H"
 
-void
 Operator::Elastic::Degradation::Degradation::Degradation(const amrex::Real WInput)
 {
 	W = WInput;
 }
 
 void
-Operator::Elastic::Degradation::Degradation::SetEta(amrex::Array<std::unique_ptr<amrex::MultiFab> > &eta,
+Operator::Elastic::Degradation::Degradation::SetEta(amrex::Vector<std::unique_ptr<amrex::MultiFab> > &eta,
 						    BC::BC &eta_bc,
-						    std::vector<AnisotropicDegradationModel *> &models)
+						    std::vector<PristineMaterialModel *> &models)
 {
   RegisterNewFab(eta,eta_bc);
   num_eta = eta[0].get()->nComp();
 	std::cout << "number of eta = " << num_eta << std::endl;
+	Cs.resize(1);
 	//Cs.resize(num_phase);
   for (int i = 0; i < AMREX_SPACEDIM; i++)
     for (int j = 0; j < AMREX_SPACEDIM; j++)
       for (int k = 0; k < AMREX_SPACEDIM; k++)
 				for (int l = 0; l < AMREX_SPACEDIM; l++)
-					Cs[i][j][k][l] = models[0]->C(i,j,k,l)
+					Cs[0][i][j][k][l] = models[0]->C(i,j,k,l);
 					//for (int n = 0; n<num_phase; n++)
 						//Cs[n][i][j][k][l] = models[n]->C(i,j,k,l);
 }
@@ -40,7 +40,7 @@ Operator::Elastic::Degradation::Degradation::C(const int i, const int j, const i
 		for (int q = 0; q < AMREX_SPACEDIM; q++)
 			for (int r = 0; r < AMREX_SPACEDIM; r++)
 				for (int s = 0; s < AMREX_SPACEDIM; s++)
-					C+ = Cs[i][j][k][l]*M(i,j,p,q,loc,amrlev,mglev,mfi)*M(k,l,r,s,loc,amrlev,mglev,mfi);
+					C += Cs[0][i][j][k][l]*M(i,j,p,q,loc,amrlev,mglev,mfi)*M(k,l,r,s,loc,amrlev,mglev,mfi);
 					//for (int n = 0; n < num_phase; n++)
 						//C += Cs[n][p][q][r][s]*M(i,j,p,q,loc,amrlev,mglev,mfi)*M(k,l,r,s,loc,amrlev,mglev,mfi);
 
@@ -65,16 +65,16 @@ Operator::Elastic::Degradation::Degradation::M(const int i, const int j, const i
 	int n = k*(k==l ? 1:0) + (1- (k==l ? 1:0))*(9 - k - l);
 
 	if (m != n) return 0;
-	if (m == 1) return eta(loc,0);
-	if (m == 2) return eta(loc,1);
-	if (m == 3) return eta(loc,2);
-	if (m == 4) return sqrt(eta(loc,1)*eta(loc,2));
-	if (m == 5) return sqrt(eta(loc,0)*eta(loc,2));
-	if (m == 6) return sqrt(eta(loc,0)*eta(loc,1));
+	if (m == 1) return etafab(loc,0);
+	if (m == 2) return etafab(loc,1);
+	if (m == 3) return etafab(loc,2);
+	if (m == 4) return sqrt(etafab(loc,1)*etafab(loc,2));
+	if (m == 5) return sqrt(etafab(loc,0)*etafab(loc,2));
+	if (m == 6) return sqrt(etafab(loc,0)*etafab(loc,1));
 	return 0;
 }
 
-std::array<amrex::Real, num_eta>
+std::array<amrex::Real, 3>
 Operator::Elastic::Degradation::Degradation::dMdEta(const int i, const int j, const int k, const int l,
 									const amrex::IntVect loc, const int amrlev, const int mglev, const MFIter &mfi) const
 {
@@ -82,30 +82,30 @@ Operator::Elastic::Degradation::Degradation::dMdEta(const int i, const int j, co
 	int m = i*(i==j ? 1:0) + (1- (i==j ? 1:0))*(9 - i - j);
 	int n = k*(k==l ? 1:0) + (1- (k==l ? 1:0))*(9 - k - l);
 
-	amrex::Real dMdE[3] = {0.0,0.0,0.0} //assuming num_eta = 3 here.
+	std::array<amrex::Real,3> dMdE = {0.0,0.0,0.0}; //assuming num_eta = 3 here.
 	if (m != n) return dMdE;
 	if (m == 1) return {1.0,0.0,0.0};
 	if (m == 2) return {0.0,1.0,0.0};
 	if (m == 3) return {0.0,0.0,1.0};
 	if (m == 4)
 	{
-		dMdE[1] = 0.5*sqrt(eta(loc,2))*pow(eta(loc,1),-1.5);
+		dMdE[1] = 0.5*sqrt(etafab(loc,2))*pow(etafab(loc,1),-1.5);
 		if(dMdE[1] != dMdE[1]) dMdE[1] = 0.0;
-		dMdE[2] = 0.5*sqrt(eta(loc,1))*pow(eta(loc,2),-1.5);
+		dMdE[2] = 0.5*sqrt(etafab(loc,1))*pow(etafab(loc,2),-1.5);
 		if(dMdE[2] != dMdE[2]) dMdE[2] = 0.0;
 	}
 	if (m == 5)
 	{
-		dMdE[0] = 0.5*sqrt(eta(loc,2))*pow(eta(loc,0),-1.5);
+		dMdE[0] = 0.5*sqrt(etafab(loc,2))*pow(etafab(loc,0),-1.5);
 		if(dMdE[0] != dMdE[0]) dMdE[0] = 0.0;
-		dMdE[2] = 0.5*sqrt(eta(loc,0))*pow(eta(loc,2),-1.5);
+		dMdE[2] = 0.5*sqrt(etafab(loc,0))*pow(etafab(loc,2),-1.5);
 		if(dMdE[2] != dMdE[2]) dMdE[2] = 0.0;
 	}
 	if (m == 6)
 	{
-		dMdE[0] = 0.5*sqrt(eta(loc,1))*pow(eta(loc,0),-1.5);
+		dMdE[0] = 0.5*sqrt(etafab(loc,1))*pow(etafab(loc,0),-1.5);
 		if(dMdE[0] != dMdE[0]) dMdE[0] = 0.0;
-		dMdE[1] = 0.5*sqrt(eta(loc,0))*pow(eta(loc,1),-1.5);
+		dMdE[1] = 0.5*sqrt(etafab(loc,0))*pow(etafab(loc,1),-1.5);
 		if(dMdE[1] != dMdE[1]) dMdE[1] = 0.0;
 	}
 	return dMdE;
@@ -118,40 +118,40 @@ Operator::Elastic::Degradation::Degradation::N(const int i, const int j, const a
 	const FArrayBox &etafab = GetFab(0,amrlev,mglev,mfi);
 	int m = i*(i==j ? 1:0) + (1- (i==j ? 1:0))*(9 - i - j);
 
-	if (m == 1) return (1-eta(loc,1))*(1+eta(loc,1));
-	if (m == 2) return (1-eta(loc,2))*(1+eta(loc,2));
-	if (m == 3) return (1-eta(loc,3))*(1+eta(loc,3));
-	if (m == 4) return sqrt((1-eta(loc,2))*(1+eta(loc,2))*(1-eta(loc,3))*(1+eta(loc,3)));
-	if (m == 5) return sqrt((1-eta(loc,1))*(1+eta(loc,1))*(1-eta(loc,3))*(1+eta(loc,3)));
-	if (m == 6) return sqrt((1-eta(loc,1))*(1+eta(loc,1))*(1-eta(loc,2))*(1+eta(loc,2)));
+	if (m == 1) return (1-etafab(loc,1))*(1+etafab(loc,1));
+	if (m == 2) return (1-etafab(loc,2))*(1+etafab(loc,2));
+	if (m == 3) return (1-etafab(loc,3))*(1+etafab(loc,3));
+	if (m == 4) return sqrt((1-etafab(loc,2))*(1+etafab(loc,2))*(1-etafab(loc,3))*(1+etafab(loc,3)));
+	if (m == 5) return sqrt((1-etafab(loc,1))*(1+etafab(loc,1))*(1-etafab(loc,3))*(1+etafab(loc,3)));
+	if (m == 6) return sqrt((1-etafab(loc,1))*(1+etafab(loc,1))*(1-etafab(loc,2))*(1+etafab(loc,2)));
 	return 0;
 }
 
-std::array<amrex::Real, num_eta>
+std::array<amrex::Real, 3>
 Operator::Elastic::Degradation::Degradation::dNdEta(const int i, const int j, const amrex::IntVect loc,
 								const int amrlev, const int mglev, const MFIter &mfi) const
 {
 	const FArrayBox &etafab = GetFab(0,amrlev,mglev,mfi);
 	int m = i*(i==j ? 1:0) + (1- (i==j ? 1:0))*(9 - i - j);
 
-	amrex::Real dNdE[3] = {0.0,0.0,0.0} //assuming num_eta = 3 here.
-	if(m==1) dNdE[0] = -2.0*eta(loc,0);
-	if(m==2) dNdE[1] = -2.0*eta(loc,1);
-	if(m==3) dNdE[2] = -2.0*eta(loc,2);
+	std::array<amrex::Real,3> dNdE = {0.0,0.0,0.0}; //assuming num_eta = 3 here.
+	if(m==1) dNdE[0] = -2.0*etafab(loc,0);
+	if(m==2) dNdE[1] = -2.0*etafab(loc,1);
+	if(m==3) dNdE[2] = -2.0*etafab(loc,2);
 	if(m==4)
 	{
-		dNdE[1] = -eta(loc,1)*sqrt((1-eta(loc,2)*eta(loc,2))/(1-eta(loc,1)*eta(loc,1)));
-		dNdE[2] = -eta(loc,2)*sqrt((1-eta(loc,1)*eta(loc,1))/(1-eta(loc,2)*eta(loc,2)));
+		dNdE[1] = -etafab(loc,1)*sqrt((1-etafab(loc,2)*etafab(loc,2))/(1-etafab(loc,1)*etafab(loc,1)));
+		dNdE[2] = -etafab(loc,2)*sqrt((1-etafab(loc,1)*etafab(loc,1))/(1-etafab(loc,2)*etafab(loc,2)));
 	}
 	if(m==5)
 	{
-		dNdE[0] = -eta(loc,0)*sqrt((1-eta(loc,2)*eta(loc,2))/(1-eta(loc,0)*eta(loc,0)));
-		dNdE[2] = -eta(loc,2)*sqrt((1-eta(loc,0)*eta(loc,0))/(1-eta(loc,2)*eta(loc,2)));
+		dNdE[0] = -etafab(loc,0)*sqrt((1-etafab(loc,2)*etafab(loc,2))/(1-etafab(loc,0)*etafab(loc,0)));
+		dNdE[2] = -etafab(loc,2)*sqrt((1-etafab(loc,0)*etafab(loc,0))/(1-etafab(loc,2)*etafab(loc,2)));
 	}
 	if(m==6)
 	{
-		dNdE[0] = -eta(loc,0)*sqrt((1-eta(loc,1)*eta(loc,1))/(1-eta(loc,0)*eta(loc,0)));
-		dNdE[1] = -eta(loc,1)*sqrt((1-eta(loc,0)*eta(loc,0))/(1-eta(loc,1)*eta(loc,1)));
+		dNdE[0] = -etafab(loc,0)*sqrt((1-etafab(loc,1)*etafab(loc,1))/(1-etafab(loc,0)*etafab(loc,0)));
+		dNdE[1] = -etafab(loc,1)*sqrt((1-etafab(loc,0)*etafab(loc,0))/(1-etafab(loc,1)*etafab(loc,1)));
 	}
 	return dNdE;
 }
@@ -159,7 +159,7 @@ Operator::Elastic::Degradation::Degradation::dNdEta(const int i, const int j, co
 void
 Operator::Elastic::Degradation::Degradation::Energies (FArrayBox& energyfab,
 						       const FArrayBox& ufab,
-						       int amrlev, const MFIter& mfi)
+						       int amrlev, int mglev, const MFIter& mfi)
 {
   const amrex::Real* DX = m_geom[amrlev][0].CellSize();
 
@@ -211,13 +211,13 @@ Operator::Elastic::Degradation::Degradation::Energies (FArrayBox& energyfab,
 										for (int r=0; r<AMREX_SPACEDIM; r++)
 											for (int s=0; s<AMREX_SPACEDIM; s++)
 											{
-												std::array<amrex::Real,num_eta> dMdE1 = dMdEta(p,q,i,j,m,amrlev,mglev,mfi);
-												std::array<amrex::Real,num_eta> dMdE2 = dMdEta(r,s,k,l,m,amrlev,mglev,mfi);
-												energyfab(m,n) += gradu[i][j]*(dMdE1[n]*Cs[p][q][r][s]*M(r,s,k,l,amrlev,mglev,mfi) +
-																					M(p,q,i,j,m,amrlev,mglev,mfi)*Cs[p][q][r][s]*dMdEta[n])*gradu[k][l];
+												std::array<amrex::Real,3> dMdE1 = dMdEta(p,q,i,j,m,amrlev,mglev,mfi);
+												std::array<amrex::Real,3> dMdE2 = dMdEta(r,s,k,l,m,amrlev,mglev,mfi);
+												energyfab(m,n) += gradu[i][j]*(dMdE1[n]*Cs[0][p][q][r][s]*M(r,s,k,l,m,amrlev,mglev,mfi) +
+																					M(p,q,i,j,m,amrlev,mglev,mfi)*Cs[0][p][q][r][s]*dMdE2[n])*gradu[k][l];
 												energyfab(m,n) *= 0.5;
 											}
-						energyfab(m,n) += 2.0*W*dNdEta(i,j,m,amrlev,mglev,mfi)[n]*N(i,j,amrlev,mglev,mfi);
+						energyfab(m,n) += 2.0*W*dNdEta(i,j,m,amrlev,mglev,mfi)[n]*N(i,j,m,amrlev,mglev,mfi);
 					}
 			}
 
