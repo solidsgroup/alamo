f5d0d2aef28c86a236096e18d1b9f390a5272e94
Brandon Runnels <brunnels@uccs.edu>
2018-06-12 17:35:45 -0600

    added database generation for python
    Former-commit-id: 0d4f0a01890be4da7871bb79ec212c2b22bf70d9

--- src/Integrator/Integrator.cpp
+++ src/Integrator/Integrator.cpp
@@ -68,7 +68,6 @@ Integrator::Integrator::Integrator ()
   if (ParallelDescriptor::IOProcessor())
     {
       amrex::UtilCreateCleanDirectory(plot_file, false);
-      WriteMetaData();
     }
 }
 
@@ -78,6 +77,7 @@ Integrator::Integrator::Integrator ()
 ///
 Integrator::Integrator::~Integrator ()
 {
+	WriteMetaData();
 }
 
 /// \fn    Integrator::MakeNewLevelFromCoarse
@@ -374,28 +374,28 @@ Integrator::Integrator::WriteMetaData() const
 {
   std::ofstream metadatafile;
   metadatafile.open(plot_file+"/metadata",std::ios_base::out);
-  metadatafile << "COMPILATION DETAILS" << std::endl;
-  metadatafile << "===================" << std::endl;
-  metadatafile << "Git commit hash:         " << METADATA_GITHASH << std::endl;
-  metadatafile << "AMReX version:           " << amrex::Version() << std::endl;
-  metadatafile << "Dimension:               " << AMREX_SPACEDIM << std::endl;
-  metadatafile << "Compiled by:             " << METADATA_USER  << std::endl;
-  metadatafile << "Platform:                " << METADATA_PLATFORM  << std::endl;
-  metadatafile << "Compiler:                " << METADATA_COMPILER  << std::endl;
-  metadatafile << "Compilation Date:        " << METADATA_DATE  << std::endl;
-  metadatafile << "Compilation Time:        " << METADATA_TIME  << std::endl;
+  metadatafile << "# COMPILATION DETAILS" << std::endl;
+  metadatafile << "# ===================" << std::endl;
+  metadatafile << "Git_commit_hash = " << METADATA_GITHASH << std::endl;
+  metadatafile << "AMReX_version = " << amrex::Version() << std::endl;
+  metadatafile << "Dimension = " << AMREX_SPACEDIM << std::endl;
+  metadatafile << "User = " << METADATA_USER  << std::endl;
+  metadatafile << "Platform = " << METADATA_PLATFORM  << std::endl;
+  metadatafile << "Compiler = " << METADATA_COMPILER  << std::endl;
+  metadatafile << "Compilation_Date = " << METADATA_DATE  << std::endl;
+  metadatafile << "Compilation_Time = " << METADATA_TIME  << std::endl;
   metadatafile << std::endl;
 
   auto starttime = std::chrono::system_clock::now();
   std::time_t now = std::chrono::system_clock::to_time_t(starttime);
-  metadatafile << "RUN DETAILS" << std::endl;
-  metadatafile << "===========" << std::endl;
-  metadatafile << "Simulation started:      " << std::ctime(&now);
-  metadatafile << "Number of processors:    " << amrex::ParallelDescriptor::NProcs() << std::endl;
+  metadatafile << "# RUN DETAILS" << std::endl;
+  metadatafile << "# ===========" << std::endl;
+  metadatafile << "Simulation_start_time = " << std::ctime(&now);
+  metadatafile << "Number_of_processors = " << amrex::ParallelDescriptor::NProcs() << std::endl;
   metadatafile << std::endl;
 
-  metadatafile << "PARAMETERS" << std::endl;
-  metadatafile << "==========" << std::endl;
+  metadatafile << "# PARAMETERS" << std::endl;
+  metadatafile << "# ==========" << std::endl;
   amrex::ParmParse pp;
   pp.dumpTable(metadatafile,true);
   
