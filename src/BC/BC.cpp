3bee0dfdbb2b5f570f3b43ada17e9e506a93c0a2
Brandon Runnels <brunnels@uccs.edu>
2018-06-19 15:42:08 -0600

    Cleaned up eigenstrain, significant update to BC class
    Former-commit-id: 27f2d6a59169b32710b95dad18c4a0cc1bff6118

--- src/BC/BC.cpp
+++ src/BC/BC.cpp
@@ -1,193 +1,189 @@
 #include "BC.H"
+#include "BC/Util.H"
 
-BC::BC::BC (amrex::Vector<amrex::Geometry> &_geom
-			,amrex::Vector<std::string> bc_hi_str
-			,amrex::Vector<std::string> bc_lo_str
-			,amrex::Vector<amrex::Real> _bc_lo_1
-			,amrex::Vector<amrex::Real> _bc_hi_1
-			,amrex::Vector<amrex::Real> _bc_lo_2
-			,amrex::Vector<amrex::Real> _bc_hi_2
+namespace BC
+{
+BC::BC (amrex::Vector<amrex::Geometry> &_geom
+				,amrex::Vector<std::string> bc_hi_str
+				,amrex::Vector<std::string> bc_lo_str
+				,amrex::Vector<amrex::Real> _bc_lo_1
+				,amrex::Vector<amrex::Real> _bc_hi_1
+				,amrex::Vector<amrex::Real> _bc_lo_2
+				,amrex::Vector<amrex::Real> _bc_hi_2
 #if AMREX_SPACEDIM > 2
-			,amrex::Vector<amrex::Real> _bc_lo_3
-			,amrex::Vector<amrex::Real> _bc_hi_3
+				,amrex::Vector<amrex::Real> _bc_lo_3
+				,amrex::Vector<amrex::Real> _bc_hi_3
 #endif
-			)
-  : geom(_geom)
-  ,bc_lo_1(_bc_lo_1), bc_hi_1(_bc_hi_1)
-  ,bc_lo_2(_bc_lo_2), bc_hi_2(_bc_hi_2)
+				)
+	: geom(_geom)
+	,bc_lo_1(_bc_lo_1), bc_hi_1(_bc_hi_1)
+	,bc_lo_2(_bc_lo_2), bc_hi_2(_bc_hi_2)
 #if AMREX_SPACEDIM > 2
-  ,bc_lo_3(_bc_lo_3), bc_hi_3(_bc_hi_3)
+	,bc_lo_3(_bc_lo_3), bc_hi_3(_bc_hi_3)
 #endif
 {
-  for (int i=0;i<BL_SPACEDIM;i++)
-    {
-      if (bc_hi_str[i] == "REFLECT_ODD"	) bc_hi[i] = REFLECT_ODD; 
-      if (bc_hi_str[i] == "INT_DIR"		) bc_hi[i] = INT_DIR;
-      if (bc_hi_str[i] == "PERIODIC"		) bc_hi[i] = INT_DIR;
-      if (bc_hi_str[i] == "REFLECT_EVEN"	) bc_hi[i] = REFLECT_EVEN;
-      if (bc_hi_str[i] == "FOEXTRAP"		) bc_hi[i] = FOEXTRAP;
-      if (bc_hi_str[i] == "EXT_DIR"		) bc_hi[i] = EXT_DIR;
-      if (bc_hi_str[i] == "DIRICHLET"		) bc_hi[i] = EXT_DIR;
-      if (bc_hi_str[i] == "HOEXTRAP"		) bc_hi[i] = HOEXTRAP;
+	for (int i=0;i<BL_SPACEDIM;i++)
+		{
+			bc_hi[i] = Util::ReadString(bc_hi_str[i]);
+			bc_lo[i] = Util::ReadString(bc_lo_str[i]);
 
-      if (bc_lo_str[i] == "REFLECT_ODD"	) bc_lo[i] = REFLECT_ODD;
-      if (bc_lo_str[i] == "INT_DIR"		) bc_lo[i] = INT_DIR;
-      if (bc_lo_str[i] == "PERIODIC"		) bc_lo[i] = INT_DIR;
-      if (bc_lo_str[i] == "REFLECT_EVEN"	) bc_lo[i] = REFLECT_EVEN;
-      if (bc_lo_str[i] == "FOEXTRAP"		) bc_lo[i] = FOEXTRAP;
-      if (bc_lo_str[i] == "EXT_DIR"		) bc_lo[i] = EXT_DIR;
-      if (bc_lo_str[i] == "DIRICHLET"		) bc_lo[i] = EXT_DIR;
-      if (bc_lo_str[i] == "HOEXTRAP"		) bc_lo[i] = HOEXTRAP;
-
-      // Added for Neumann BC.
-      if (bc_hi_str[i] == "NEUMANN"		) bc_hi[i] = NEUMANN;
-      if (bc_lo_str[i] == "NEUMANN"		) bc_lo[i] = NEUMANN;
-    }
+			if (Util::IsPeriodic(bc_lo[i]) != Util::IsPeriodic(bc_hi[i]))
+				amrex::Abort("Invalid BCs cannot be periodic on one side and not the other");
+		}
 }
 
 void
-BC::BC::FillBoundary (amrex::MultiFab& mf, int, int, amrex::Real /*time*/) 
+BC::FillBoundary (amrex::MultiFab& mf, int, int, amrex::Real /*time*/) 
 {
-  if ((bc_lo[0] == NEUMANN || bc_lo[0] == EXT_DIR) && bc_lo_1.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_1");
-  if ((bc_hi[0] == NEUMANN || bc_hi[0] == EXT_DIR) && bc_hi_1.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_1");
-  if ((bc_lo[1] == NEUMANN || bc_lo[1] == EXT_DIR) && bc_lo_2.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_2");
-  if ((bc_hi[1] == NEUMANN || bc_hi[1] == EXT_DIR) && bc_hi_2.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_2");
+	if ((Util::IsNeumann(bc_lo[0]) || Util::IsDirichlet(bc_lo[0])) && bc_lo_1.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_1");
+	if ((Util::IsNeumann(bc_hi[0]) || Util::IsDirichlet(bc_hi[0])) && bc_hi_1.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_1");
+	if ((Util::IsNeumann(bc_lo[1]) || Util::IsDirichlet(bc_lo[1])) && bc_lo_2.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_2");
+	if ((Util::IsNeumann(bc_hi[1]) || Util::IsDirichlet(bc_hi[1])) && bc_hi_2.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_2");
 #if AMREX_SPACEDIM>2
-  if ((bc_lo[2] == NEUMANN || bc_lo[2] == EXT_DIR) && bc_lo_3.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_3");
-  if ((bc_hi[2] == NEUMANN || bc_hi[2] == EXT_DIR) && bc_hi_3.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_3");
+	if ((Util::IsNeumann(bc_lo[2]) || Util::IsDirichlet(bc_lo[2])) && bc_lo_3.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_lo_3");
+	if ((Util::IsNeumann(bc_hi[2]) || Util::IsDirichlet(bc_hi[2])) && bc_hi_3.size() < mf.nComp()) amrex::Abort("Not enough values specified for bc_hi_3");
 #endif
 
-  amrex::Box domain(geom[lev].Domain());
+	amrex::Box domain(geom[lev].Domain());
 
-  mf.FillBoundary(geom[lev].periodicity());
+	mf.FillBoundary(geom[lev].periodicity());
 
-  // Added for Neumann BC
-  const amrex::Real* dx = geom[lev].CellSize();
+	// Added for Neumann BC
+	const amrex::Real* dx = geom[lev].CellSize();
 
-  for (amrex::MFIter mfi(mf,true); mfi.isValid(); ++mfi)
-    {
-      const amrex::Box& box = mfi.tilebox();
+	for (amrex::MFIter mfi(mf,true); mfi.isValid(); ++mfi)
+		{
+			const amrex::Box& box = mfi.tilebox();
 
-      amrex::BaseFab<amrex::Real> &phi_box = mf[mfi];
+			amrex::BaseFab<amrex::Real> &phi_box = mf[mfi];
 
-      for (int i = box.loVect()[0] - mf.nGrow(); i<=box.hiVect()[0] + mf.nGrow(); i++)
-	for (int j = box.loVect()[1] - mf.nGrow(); j<=box.hiVect()[1] + mf.nGrow(); j++)
+			for (int i = box.loVect()[0] - mf.nGrow(); i<=box.hiVect()[0] + mf.nGrow(); i++)
+				for (int j = box.loVect()[1] - mf.nGrow(); j<=box.hiVect()[1] + mf.nGrow(); j++)
 #if BL_SPACEDIM>2
-	  for (int k = box.loVect()[2] - mf.nGrow(); k<=box.hiVect()[2] + mf.nGrow(); k++)
+					for (int k = box.loVect()[2] - mf.nGrow(); k<=box.hiVect()[2] + mf.nGrow(); k++)
 #endif
-	    for (int n = 0; n < mf.nComp(); n++)
-	      {
-		if (i < domain.loVect()[0]) // Left boundary
-		  {
-		    if (bc_lo[0] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_1[n];
-		      }
-		    else if(bc_lo[0] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i+1,j,k)),n) - bc_lo_1[n]*dx[0];
-		      }
-                    else if(bc_lo[0] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(-i+box.loVect()[0]+box.hiVect()[0],j,k)),n);
-		      }
-		  }
+						for (int n = 0; n < mf.nComp(); n++)
+							{
+								if (i < domain.loVect()[0]) // Left boundary
+									{
+										if (Util::IsDirichlet(bc_lo[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_1[n];
+											}
+										else if(Util::IsNeumann(bc_lo[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i+1,j,k)),n) - bc_lo_1[n]*dx[0];
+											}
+										else if(Util::IsPeriodic(bc_lo[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(-i+box.loVect()[0]+box.hiVect()[0],j,k)),n);
+											}
+									}
 
-		if (i > domain.hiVect()[0]) // Right boundary
-		  {
-		    if (bc_hi[0] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_1[n];
-		      }
-		    else if(bc_hi[0] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i-1,j,k)),n) - bc_hi_1[n]*dx[0];
-		      }
-                    else if(bc_hi[0] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(-i+box.loVect()[0]+box.hiVect()[0],j,k)),n);
-		      }
-		  }
+								if (i > domain.hiVect()[0]) // Right boundary
+									{
+										if (Util::IsDirichlet(bc_hi[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_1[n];
+											}
+										else if(Util::IsNeumann(bc_hi[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i-1,j,k)),n) - bc_hi_1[n]*dx[0];
+											}
+										else if(Util::IsPeriodic(bc_hi[0]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(-i+box.loVect()[0]+box.hiVect()[0],j,k)),n);
+											}
+									}
 
-		if (j < domain.loVect()[1]) // Bottom boundary
-		  {
-		    if (bc_lo[1] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_2[n];
-		      }
-		    else if (bc_lo[1] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j+1,k)),n) - bc_lo_2[n]*dx[1];
-		      }
-                    else if(bc_lo[1] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(i,-j+box.loVect()[1]+box.hiVect()[1],k)),n);
-		      }
-		  }
+								if (j < domain.loVect()[1]) // Bottom boundary
+									{
+										if (Util::IsDirichlet(bc_lo[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_2[n];
+											}
+										else if (Util::IsNeumann(bc_lo[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j+1,k)),n) - bc_lo_2[n]*dx[1];
+											}
+										else if(Util::IsPeriodic(bc_lo[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(i,-j+box.loVect()[1]+box.hiVect()[1],k)),n);
+											}
+									}
 
-		if (j > domain.hiVect()[1]) // Top boundary
-		  {
-		    if (bc_hi[1] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_2[n];
-		      }
-		    else if (bc_hi[1] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j-1,k)),n) - bc_hi_2[n]*dx[1];
-		      }
-                    else if(bc_hi[1] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(i,-j+box.loVect()[1]+box.hiVect()[1],k)),n);
-		      }
-		  }
+								if (j > domain.hiVect()[1]) // Top boundary
+									{
+										if (Util::IsDirichlet(bc_hi[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_2[n];
+											}
+										else if (Util::IsNeumann(bc_hi[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j-1,k)),n) - bc_hi_2[n]*dx[1];
+											}
+										else if(Util::IsPeriodic(bc_hi[1]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(i,-j+box.loVect()[1]+box.hiVect()[1],k)),n);
+											}
+									}
 
 
 #if AMREX_SPACEDIM>2
-		if (k < domain.loVect()[2])
-		  {
-		    if (bc_lo[2] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_3[n];
-		      }
-		    else if (bc_lo[2] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k+1)),n) - bc_lo_3[n]*dx[2];
-		      }
-                    else if(bc_lo[2] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(i,j,-k+box.loVect()[2]+box.hiVect()[2])),n);
-		      }
-		  }
+								if (k < domain.loVect()[2])
+									{
+										if (Util::IsDirichlet(bc_lo[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_lo_3[n];
+											}
+										else if (Util::IsNeumann(bc_lo[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k+1)),n) - bc_lo_3[n]*dx[2];
+											}
+										else if(Util::IsPeriodic(bc_lo[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(i,j,-k+box.loVect()[2]+box.hiVect()[2])),n);
+											}
+									}
 
-		if (k > domain.hiVect()[2])
-		  {
-		    if (bc_hi[2] == EXT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_3[n];
-		      }
-		    else if(bc_hi[2] == NEUMANN)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k-1)),n) - bc_hi_3[n]*dx[2];
-		      }
-                    else if(bc_hi[2] == INT_DIR)
-		      {
-			phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
-				phi_box(amrex::IntVect(AMREX_D_DECL(i,j,-k+box.loVect()[2]+box.hiVect()[2])),n);
-		      }
-		  }
+								if (k > domain.hiVect()[2])
+									{
+										if (Util::IsDirichlet(bc_hi[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = bc_hi_3[n];
+											}
+										else if(Util::IsNeumann(bc_hi[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k-1)),n) - bc_hi_3[n]*dx[2];
+											}
+										else if(Util::IsPeriodic(bc_hi[2]))
+											{
+												phi_box(amrex::IntVect(AMREX_D_DECL(i,j,k)),n) = 
+													phi_box(amrex::IntVect(AMREX_D_DECL(i,j,-k+box.loVect()[2]+box.hiVect()[2])),n);
+											}
+									}
 #endif
 
-	      }
-    }
+							}
+		}
 }
 
 void
-BC::BC::SetLevel(int _lev) {lev=_lev;}
+BC::SetLevel(int _lev) {lev=_lev;}
 
 amrex::BCRec
-BC::BC::GetBCRec() {return amrex::BCRec(bc_lo,bc_hi);}
+BC::GetBCRec() {return amrex::BCRec(bc_lo,bc_hi);}
+
+amrex::Array<int,AMREX_SPACEDIM>
+BC::IsPeriodic()
+{
+	return {AMREX_D_DECL(Util::IsPeriodic(bc_lo[0]),Util::IsPeriodic(bc_lo[1]),Util::IsPeriodic(bc_lo[2]))};
+}
+
+
+}
