7a4a19de19938c00762fd0661f92a81e7dfc8ba0
Brandon Runnels <brunnels@uccs.edu>
2019-05-09 11:50:18 -0600

    fixed compile error
    Former-commit-id: 70214a041cc24b392c431e008b437423304ede40

--- src/Test/Operator/Elastic_UniaxialTest.cpp
+++ src/Test/Operator/Elastic_UniaxialTest.cpp
@@ -15,7 +15,7 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
         Set::Scalar tolerance = 0.01;
 
 	int failed = 0;
-	
+
 	using model_type = Model::Solid::LinearElastic::Isotropic;
 	Set::Scalar lame=2.6, shear=6.0;
 	model_type model(lame,shear);
@@ -26,7 +26,6 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
  	for (int ilev = 0; ilev < nlevels; ++ilev) modelfab[ilev].define(ngrids[ilev], dmap[ilev], 1, 2);
  	for (int ilev = 0; ilev < nlevels; ++ilev) modelfab[ilev].setVal(model);
 
-	
 	Set::Vector vec(AMREX_D_DECL(0.0, 0.0, 0.0));
 	vec[component]=0.1;
 	
@@ -68,9 +67,8 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
 		       bc.Set(bc.Face::ZLO, bc.Direction::X, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
 		       bc.Set(bc.Face::ZHI, bc.Direction::X, bc.Type::Neumann, 0.0, rhs_prescribed, geom););
 	}
-	
-	
-	if (component==1)
+#if AMREX_SPACEDIM > 1	
+	else if (component==1)
 	{
 	  AMREX_D_TERM(,
 		       bc.Set(bc.Face::XLO, bc.Direction::Y, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
@@ -82,9 +80,16 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
 		       bc.Set(bc.Face::ZLO, bc.Direction::Y, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
 		       bc.Set(bc.Face::ZHI, bc.Direction::Y, bc.Type::Neumann, 0.0, rhs_prescribed, geom););
 	}
-	
-	
-	if (component==2 && AMREX_SPACEDIM>2) //3D
+#endif
+#if AMREX_SPACEDIM > 2	
+	//if (component==2 && AMREX_SPACEDIM>2) // <<<< This is not a compile-time directive, which means it will
+	//                                              still show up when you compile in 2D.
+	//                                              Please take a look at the presentation
+	//                                                  https://solids.uccs.edu/teaching/bootcamp/Tutorial2/files/Tutorial2.pdf
+	//                                              and read the "C Preprocessor" section.
+	//                                              Delete this comment once you're sure you understand why what
+	//                                              you had would not work.
+	if (component==2 && AMREX_SPACEDIM>2) //3D 
 	{
 	  bc.Set(bc.Face::XLO, bc.Direction::Z, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
 	  bc.Set(bc.Face::XHI, bc.Direction::Z, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
@@ -94,7 +99,7 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
 	  bc.Set(bc.Face::ZHI, bc.Direction::Y, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
 	  bc.Set(bc.Face::ZHI, bc.Direction::Z, bc.Type::Displacement, 0.1, rhs_prescribed, geom);
 	}
-	
+#endif	
 	
 	//bc.Set(bc.Face::XHI, bc.Direction::X, bc.Type::Displacement, 0.1, rhs_prescribed, geom);
 	//bc.Set(bc.Face::YLO, bc.Direction::X, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
@@ -103,6 +108,8 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
 	//bc.Set(bc.Face::YHI, bc.Direction::Y, bc.Type::Neumann, 0.0, rhs_prescribed, geom);
 	
 	elastic.SetBC(&bc);
+
+
 
 	amrex::MLMG mlmg(elastic);
 	// mlmg.setMaxIter(100);
@@ -125,9 +132,17 @@ int Elastic::UniaxialTest(int verbose, int component, int n, std::string plotfil
 	
 	Set::Scalar tol_rel = 1E-8;
 	Set::Scalar tol_abs = 0;
-        #if (component!=0)
-	  component-=component;
-        #endif  
+        //#if (component!=0) // <<< You are using a preprocessor directive here instead 
+	//                          of a regular if statement. This will cause the code
+	//                          to ALWAYS execute this stement.
+	//                          Once you understand what was wrong with this, you can
+	//                          delete this comment.
+        if (component!=0)
+	{
+		component-=component;
+	}
+        //#endif  
+         
  	mlmg.solve(GetVecOfPtrs(solution_numeric), GetVecOfConstPtrs(rhs_prescribed), tol_rel,tol_abs);
 
 	// Compute solution error
