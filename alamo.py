4adc09ef6c7bb4ae326a4ee86742b7ea7f546628
Brandon Runnels <brunnels@uccs.edu>
2019-05-13 07:22:55 -0600

    updated python interface

--- alamo.py
+++ alamo.py
@@ -1,12 +1,13 @@
 import alamo
 import sys
 
-print(sys.argv)
+#print(sys.argv)
 alamo.Initialize()
 mytest = alamo.Elastic()
 
-mytest.Define(16,2)
+mytest.Define(16,3)
 
 mytest.setBounds(1.0,1.0,1.0)
 
-mytest.TrigTest(3,0,1,"testoutput")
+ret = mytest.TrigTest(3,0,1,"testoutput")
+print(ret)
