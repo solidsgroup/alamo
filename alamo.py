61da12b6b15d913dd8cc6fba2c2243a8edfd52ba
Brandon Runnels <brunnels@uccs.edu>
2019-05-14 09:44:35 -0600

    have two-way array/list conversion working with python

--- alamo.py
+++ alamo.py
@@ -1,13 +1,16 @@
 import alamo
 import sys
 
-#print(sys.argv)
 alamo.Initialize()
 mytest = alamo.Elastic()
 
-mytest.Define(16,3)
+#mytest.Define(16,3)
+#print(alamo.Grid.values)
+mytest.Define([32,32,32],3,1,mytest.Grid.XYZ)
 
-mytest.setBounds(1.0,1.0,1.0)
+#mytest.setBounds(1.0,1.0,1.0)
+mytest.setBounds([1.0,1.0,1.0])
+
 
 ret = mytest.TrigTest(3,0,1,"testoutput")
-print(ret)
+print("Return value = " + str(ret))
