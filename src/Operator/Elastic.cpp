b277e2383b35851f977cbb44dd71ae5938321a54
Dymonight <dmorris%@uccs.edu>
2019-05-29 13:34:13 -0600

    Fixed code typo
    Former-commit-id: 8a4d82a135a57aa6b5fe066526ecbd5e6209d48e

--- src/Operator/Elastic.cpp
+++ src/Operator/Elastic.cpp
@@ -525,7 +525,7 @@ Elastic<T>::Energy (int amrlev,
 						     		 gradu(p,2) = (Numeric::Stencil<Set::Scalar,0,0,1>::D(u, i,j,k,p, DX, sten)););
 					    }
 
-					 	Set::Matrix eps = .5*(gradu + gradu.transpose)
+					 	// Set::Matrix eps = .5*(gradu + gradu.transpose)
 					    Set::Matrix sig = C(i,j,k)(gradu);
 
 					    energy(i,j,k) = (gradu.transpose() * sig).trace();
