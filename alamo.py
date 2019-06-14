2dd41252cc529c0056bf429195370b7eb0e3ea43
dbugas <dbugas@uccs.edu>
2019-06-14 13:25:48 -0600

    added slip systems and a way to manage if they're on or off

--- alamo.py
+++ alamo.py
@@ -1,5 +1,6 @@
 import alamo
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Initialize Alamo
 alamo.Util.Initialize()
@@ -20,15 +21,26 @@ model.Define(lamb,mu)
 # or the library will break
 eps = np.ndarray(shape=(3,3))
 eps *= 0
-eps[0][0] = 0.1
+eps[0][0] = 0
+stressx = []
+strainx = []
+for i in range(10):
+	eps[0][0] = eps[0][0] + 0.01
+	#eps[1][2] = 0
+	print(eps)
+	strainx.append(eps[0][0])
+	# Compute the strain energy as defined by the model
+	energy = model.W(eps)
 
-# Compute the strain energy as defined by the model
-energy = model.W(eps)
-print(energy)
+	# Compute the stress tensor as defined by the model
+	sigma = model.DW(eps)
+	print(sigma[0][0])
+	stressx.append(sigma[0][0])
 
-# Compute the stress tensor as defined by the model
-sigma = model.DW(eps)
-print(sigma)
-
+plt.plot(strainx, stressx)
+plt.xlabel('Strain')
+plt.ylabel('Stress')
+plt.title('Linear elastic isotropic stress-strain curve')
+plt.show()
 # Finalize Alamo
 alamo.Util.Finalize()
