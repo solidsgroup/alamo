5df395725c4fffa3d41d47df5e089603ef9e0650
Brandon Runnels <brunnels@uccs.edu>
2019-07-10 13:02:47 -0600

    simba updates

--- simba/web.py
+++ simba/web.py
@@ -45,8 +45,9 @@ def requires_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if not auth or not check_auth(auth.username, auth.password):
-            return authenticate()
+        if (pwd and usr):
+            if not auth or not check_auth(auth.username, auth.password):
+                return authenticate()
         return f(*args, **kwargs)
     return decorated    
 
