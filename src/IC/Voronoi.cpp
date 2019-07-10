3bc4576f3442a3c51ea3456481e75299c6168887
Dymonight <dmorris%@uccs.edu>
2019-07-10 10:57:16 -0600

    fixes to IC files after restructuring

--- src/IC/Voronoi.cpp
+++ src/IC/Voronoi.cpp
@@ -28,7 +28,7 @@ Voronoi::Voronoi (amrex::Vector<amrex::Geometry> &_geom, int _number_of_grains)
     }
 }
   
-void Voronoi::Add(const int lev, amrex::Vector<amrex::MultiFab * > &field)
+void IC::Voronoi::Add(const int lev, amrex::Vector<amrex::MultiFab * > &field)
 {
   AMREX_D_TERM(amrex::Real sizex = geom[0].ProbHi()[0] - geom[0].ProbLo()[0];,
 	       amrex::Real sizey = geom[0].ProbHi()[1] - geom[0].ProbLo()[1];,
