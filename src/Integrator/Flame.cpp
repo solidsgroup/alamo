8b7da8aab8dd171a2180e9516bd48fadd88e4d16
Dymonight <dmorris%@uccs.edu>
2019-08-10 13:53:42 -0600

    IC input revisions
    Former-commit-id: 25a745ed66e9d4084972c5e93db572daf2c34a8c

--- src/Integrator/Flame.cpp
+++ src/Integrator/Flame.cpp
@@ -68,8 +68,8 @@ Flame::Flame () : Integrator()
   for (int i = 0; i < fs_number; i++) fs[i] = 0.5*(1.0 + Util::Random());
   VoronoiIC->Define(fs_number,fs,IC::Voronoi::Type::Values);
 
-  eta_ic = new IC::Wedge(geom);
-  // eta_ic = new IC::Constant(geom,value);
+  EtaIC = new IC::Wedge(geom);
+  // EtaIC = new IC::Constant(geom,value);
 
   RegisterNewFab(Temp,     TempBC, 1, 1, "Temp");
   RegisterNewFab(Temp_old, TempBC, 1, 1, "Temp_old");
@@ -99,8 +99,8 @@ void Flame::Initialize (int lev)
 					Temp_old_box(amrex::IntVect(AMREX_D_DECL(i,j,k))) =  0.0;
 				}
 		}
-	eta_ic->Initialize(lev,Eta);
-	eta_ic->Initialize(lev,Eta_old);
+	EtaIC->Initialize(lev,Eta);
+	EtaIC->Initialize(lev,Eta_old);
 	
 	VoronoiIC->Initialize(lev,FlameSpeedFab);
 }
