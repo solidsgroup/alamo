cda27b57daf88e91f47bb7cd377e0eac655463ab
Brandon Runnels <brunnels@uccs.edu>
2019-09-30 16:34:23 -0600

    updating analyze script

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -14,30 +14,58 @@ print("This script is still in progress!")
 abspath = os.path.abspath(args.file)
 print(abspath)
 
+f = open(args.file)
+f.readline() #Hyperclaw stuff
+nfields = int(f.readline());  print("# fields:     " + str(nfields))
+for n in range(0,nfields): f.readline()
+f.readline() # Spatial dimension maybe?
+f.readline() # Time maybe?
+namrlevs = int(f.readline()); print("# AMR Levels: " + str(namrlevs))
+domainlo = [float(d) for d in f.readline().replace(' \n','').split(' ')]; print("Domain Lo Vect: ", domainlo)
+domainhi = [float(d) for d in f.readline().replace(' \n','').split(' ')]; print("Domain Hi Vect: ", domainhi)
+rootvolume = (domainhi[0]-domainlo[0])*(domainhi[1]-domainlo[1])*(domainhi[2]-domainlo[2])
+f.readline() # List of ref ratios for all AMR levels
+f.readline() # List of int domains for all AMR levels
+f.readline() # List of refinements for all AMR levels
+dx = []
+for n in range(0,namrlevs+1): 
+    dx.append([float(d) for d in f.readline().replace(' \n','').split(' ')])
+
 rootdir = os.path.dirname(abspath)
 print(rootdir)
 
-for leveldir in sorted(glob(rootdir+"/Level*")):
-    print(leveldir.replace(rootdir+"/",""))
+#for leveldir in sorted(glob(rootdir+"/Level*")):
+print('{:>10}'.format("Level"),end="")
+print('{:>15}'.format("# Nodes"),end="")
+print('{:>15}'.format("# Patches"),end="")
+print('{:>20}'.format("Volume"),end="")
+print('{:>20}'.format("% Volume"),end="")
+print()
+
+for amrlev in range(0,namrlevs+1):
+    leveldir = rootdir+"/Level_"+str(amrlev)+"/"
+    #print(leveldir)
     f = open(leveldir + "/Cell_H")
     nodes = 0
-    patches = 0
+    patches = 0 
+    volume = 0
     for line in f.readlines():
         exp = re.compile('\(\(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\)\)')
         res = exp.findall(line)
         if len(res) == 0 : continue
-        #print(res[0])
         dimx = int(res[0][3])-int(res[0][0])
         dimy = int(res[0][4])-int(res[0][1])
         dimz = int(res[0][5])-int(res[0][2])
+        volume += dimx*dx[amrlev][0] * dimy*dx[amrlev][1] * dimz*dx[amrlev][2]
         #print(dimx,dimy,dimz,dimx*dimy*dimz)
         patches +=1 
         nodes += dimx*dimy*dimz
-    print(nodes," nodes")
-    print(patches, " patches")
-
-    #exit()
-
+    print('{:>10}'.format(amrlev),end="")
+    print('{:>15}'.format(nodes),end="")
+    print('{:>15}'.format(patches),end="")
+    print('{:>20.5}'.format(volume),end="")
+    print('{:>20.5}'.format(100*volume/rootvolume),end="")
+    print()
 
 
 
