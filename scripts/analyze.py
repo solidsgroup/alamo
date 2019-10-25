b78a3ac5e50a27e1f6134b046543662926fbd2df
Brandon Runnels <brunnels@uccs.edu>
2019-10-25 17:45:44 -0600

    updated analyze script to handle 2D
    Former-commit-id: e3f2462ca88670386ecd025f88167bbc47e8c9cb

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -18,12 +18,15 @@ f = open(args.file)
 f.readline() #Hyperclaw stuff
 nfields = int(f.readline());  print("# fields:     " + str(nfields))
 for n in range(0,nfields): f.readline()
-f.readline() # Spatial dimension maybe?
+dimension = int(f.readline()) # Spatial dimension maybe?
+print("Dimension: ", dimension)
 f.readline() # Time maybe?
 namrlevs = int(f.readline()); print("# AMR Levels: " + str(namrlevs))
 domainlo = [float(d) for d in f.readline().replace(' \n','').split(' ')]; print("Domain Lo Vect: ", domainlo)
 domainhi = [float(d) for d in f.readline().replace(' \n','').split(' ')]; print("Domain Hi Vect: ", domainhi)
-rootvolume = (domainhi[0]-domainlo[0])*(domainhi[1]-domainlo[1])*(domainhi[2]-domainlo[2])
+rootvolume = (domainhi[0]-domainlo[0])
+if dimension > 1: rootvolume *= (domainhi[1]-domainlo[1])
+if dimension > 2: rootvolume *= (domainhi[2]-domainlo[2])
 f.readline() # List of ref ratios for all AMR levels
 f.readline() # List of int domains for all AMR levels
 f.readline() # List of refinements for all AMR levels
@@ -50,16 +53,26 @@ for amrlev in range(0,namrlevs+1):
     patches = 0 
     volume = 0
     for line in f.readlines():
-        exp = re.compile('\(\(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\)\)')
+        
+        if dimension == 2: 
+            exp = re.compile('\(\(([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*)\)\)')
+        if dimension == 3:
+            exp = re.compile('\(\(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\) \(([0-9]*),([0-9]*),([0-9]*)\)\)')
         res = exp.findall(line)
         if len(res) == 0 : continue
-        dimx = int(res[0][3])-int(res[0][0])
-        dimy = int(res[0][4])-int(res[0][1])
-        dimz = int(res[0][5])-int(res[0][2])
-        volume += dimx*dx[amrlev][0] * dimy*dx[amrlev][1] * dimz*dx[amrlev][2]
-        #print(dimx,dimy,dimz,dimx*dimy*dimz)
+        if dimension == 2:
+            dimx = int(res[0][2])-int(res[0][0])
+            dimy = int(res[0][3])-int(res[0][1])
+            volume += dimx*dx[amrlev][0] * dimy*dx[amrlev][1]
+            nodes += dimx*dimy
+        if dimension == 3:
+            dimx = int(res[0][3])-int(res[0][0])
+            dimy = int(res[0][4])-int(res[0][1])
+            dimz = int(res[0][5])-int(res[0][2])
+            volume += dimx*dx[amrlev][0] * dimy*dx[amrlev][1] * dimz*dx[amrlev][2]
+            nodes += dimx*dimy*dimz
         patches +=1 
-        nodes += dimx*dimy*dimz
+        
     print('{:>10}'.format(amrlev),end="")
     print('{:>15}'.format(nodes),end="")
     print('{:>15}'.format(patches),end="")
