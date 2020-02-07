0dee091d75149fdee399976c4e144fa3069d7ebd
dbugas <dbugas@uccs.edu>
2020-02-07 11:37:30 -0700

    Organizing3

--- src/Model/Solid/LinearElastic/CrystalPlastic.cpp
+++ src/Model/Solid/LinearElastic/CrystalPlastic.cpp
@@ -231,10 +231,8 @@ CrystalPlastic::define(Set::Scalar C11, Set::Scalar C12, Set::Scalar C44, Set::S
 void
 CrystalPlastic::define(Set::Scalar C11, Set::Scalar C12, Set::Scalar C44, Eigen::Matrix3d R)
 {
-  
 	amrex::Real Ctmp[3][3][3][3];
-	amrex::Real Crot[3][3][3][3];
-	C = Set::Matrix4<3,Set::Sym::MajorMinor>::Zero();
+	ddw = Set::Matrix4<AMREX_SPACEDIM,Set::Sym::MajorMinor>::Zero();
 
 	for(int i = 0; i < 3; i++) 
 		for(int j = 0; j < 3; j++) 
@@ -246,47 +244,19 @@ CrystalPlastic::define(Set::Scalar C11, Set::Scalar C12, Set::Scalar C44, Eigen:
 					else if (i==j && k==l) Ctmp[i][j][k][l] = C12;
 					else Ctmp[i][j][k][l] = 0.0;
 				}
-	for(int p = 0; p < 3; p++) 
-		for(int q = 0; q < 3; q++) 
-			for(int s = 0; s < 3; s++) 
-				for(int t = 0; t < 3; t++)
+	for(int p = 0; p < AMREX_SPACEDIM; p++) 
+		for(int q = 0; q < AMREX_SPACEDIM; q++) 
+			for(int s = 0; s < AMREX_SPACEDIM; s++) 
+				for(int t = 0; t < AMREX_SPACEDIM; t++)
 				{
-					C(p,q,s,t) = 0.0;
+					ddw(p,q,s,t) = 0.0;
 					for(int i = 0; i < 3; i++) 
 						for(int j = 0; j < 3; j++) 
 							for(int k = 0; k < 3; k++) 
 								for(int l = 0; l < 3; l++) 
-									C(p,q,s,t) += R(p,i)*R(s,k)*Ctmp[i][j][k][l]*R(q,j)*R(t,l);
-				}
+									ddw(p,q,s,t) += R(p,i)*R(s,k)*Ctmp[i][j][k][l]*R(q,j)*R(t,l);
 }
-void
-CrystalPlastic::Randomize()
-{
-	Set::Scalar C11 = 0.5 + 0.5*Util::Random();
-	Set::Scalar C12 = 0.5 + 0.5*Util::Random();
-	Set::Scalar C44 = 0.5 + 0.5*Util::Random();
-
-	Set::Scalar phi1 = 2.0*Set::Constant::Pi * Util::Random();
-	Set::Scalar Phi  = 2.0*Set::Constant::Pi * Util::Random();
-	Set::Scalar phi2 = 2.0*Set::Constant::Pi * Util::Random();
-
-	define(C11,C12,C44,phi1,Phi,phi2);
-}
-Set::Scalar 
-CrystalPlastic::W(Set::Matrix &gradu) const
-{
-	Set::Matrix sig = C*gradu;
-	return 0.5 * (sig*gradu).trace();
-}
-Set::Matrix CrystalPlastic::operator () (Set::Matrix &gradu,bool) const
-{
-	return C*gradu;
-}
-Set::Matrix
-CrystalPlastic::DW (Set::Matrix &gradu) const
-{
-	return (*this)(gradu);
-}
+/*
 Set::Vector
 CrystalPlastic::operator () (std::array<Set::Matrix,AMREX_SPACEDIM> &gradgradu,bool)
 {
@@ -298,15 +268,7 @@ CrystalPlastic::operator () (std::array<Set::Matrix,AMREX_SPACEDIM> &gradgradu,b
 					ret(i) += C(i,j,k,l)*gradgradu[k](l,j);
 	return ret;
 }
-Set::Vector
-CrystalPlastic::DW (std::array<Set::Matrix,AMREX_SPACEDIM> &gradgradu)
-{
-	return (*this)(gradgradu);
-}
-Set::Matrix4<3,Set::Sym::MajorMinor>
-CrystalPlastic::DDW(Set::Matrix &/*gradu*/) const
-{
-	return C;
+*/
 }
 }
 }
