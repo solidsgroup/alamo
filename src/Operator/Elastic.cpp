63a2b6b51e8dff317cca0d5d60ae82e024138b9a
Brandon Runnels <brunnels@uccs.edu>
2020-04-20 13:58:18 -0600

    Integrated 'disconnection' branch improvements to Newton solver, fixed BC issue
    Former-commit-id: 1bc3c31ec75feaefd27c91da137d9d31574ab29b

--- src/Operator/Elastic.cpp
+++ src/Operator/Elastic.cpp
@@ -559,7 +559,7 @@ Elastic<T>::Energy (int amrlev,
 
 					    // energy(i,j,k) = (gradu.transpose() * sig).trace();
 						
-						energy(i,j,k) = 0;
+						energy(i,j,k) = C(i,j,k).W(gradu);
 						for (int m = 0; m < AMREX_SPACEDIM; m++)
 						{
 							for(int n = 0; n < AMREX_SPACEDIM; n++)
@@ -592,6 +592,7 @@ Elastic<T>::Energy (int amrlev, amrex::MultiFab& a_energies, const amrex::MultiF
 	{
 		const Box& bx = mfi.tilebox();
 		amrex::Array4<amrex::Real> const& energies   = a_energies.array(mfi);
+		//amrex::Array4<amrex::Real> const& u  = a_u.array(mfi);
 		amrex::Array4<const amrex::Real> const& u  = a_u.array(mfi);
 		amrex::ParallelFor (bx,[=] AMREX_GPU_DEVICE(int i, int j, int k)
 	    {
@@ -605,19 +606,20 @@ Elastic<T>::Energy (int amrlev, amrex::MultiFab& a_energies, const amrex::MultiF
 			     		 gradu(p,1) = (Numeric::Stencil<Set::Scalar,0,1,0>::D(u, i,j,k,p, DX, sten));,
 			     		 gradu(p,2) = (Numeric::Stencil<Set::Scalar,0,0,1>::D(u, i,j,k,p, DX, sten)););
 		    }
-		 	Set::Matrix eps = .5 * (gradu + gradu.transpose());
+		 	//Set::Matrix eps = .5 * (gradu + gradu.transpose());
 			 
 			for (unsigned int p = 0; p < a_models.size(); p++)
 			{
-		    	Set::Matrix sig = a_models[p](gradu,m_homogeneous);
-				energies(i,j,k,p) = 0;
-				for (int m = 0; m < AMREX_SPACEDIM; m++)
-				{
-					for(int n = 0; n < AMREX_SPACEDIM; n++)
-					{
-						energies(i,j,k,p) += .5 * sig(m,n) * eps(m,n);
-					}
-				}
+		    	//Set::Matrix sig = a_models[p](gradu,m_homogeneous);
+				energies(i,j,k,p) = a_models[p].W(gradu);
+				//energies(i,j,k,p) = 0;
+				//for (int m = 0; m < AMREX_SPACEDIM; m++)
+				//{
+				//	for(int n = 0; n < AMREX_SPACEDIM; n++)
+				//	{
+				//		//.5 * sig(m,n) * gradu(m,n);
+				//	}
+				//}
 			}
 	    });
 	}
