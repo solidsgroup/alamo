79879dd1d5fb4ec38a48e6df6e1a9cd82c202a81
Baburaj Kangarajan <bkanagar@uccs.edu>
2020-05-26 06:50:56 -0600

    commit before merging

--- simba/templates/datatable.js
+++ simba/templates/datatable.js
@@ -2,11 +2,36 @@ $(document).ready(function() {
     var table = $('#bigtable').DataTable( {
 		"scrollX": true,
 		"lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ],
-		stateSave: true,
-		buttons: ['copy','colvis'],
+    "stateSave": true,
+    "stateDuration": 31556952,
+    buttons: ['copy','colvis',
+                {
+                  text:'Hide Similar Columns',
+                  action: function()
+                  {
+                    table.columns().every( function ( ) {
+                      var data = this.data().unique();
+                      if(data.length == 1)
+                      {
+                        console.log(data.length);
+                        this.visible(false);
+                      }
+                    } );
+                  }
+                
+                },
+                {
+                  text:'Show All Columns',
+                  action: function()
+                  {
+                    table.columns().every( function ( ) {
+                      this.visible(true);
+                    } );
+                  }
+                
+                }
+              ],
     } );
     table.buttons().container()
         .appendTo( '#bigtable_wrapper .col-sm-6:eq(0)' );
 });
-
-
