ab211a6931d134f0297f6360c5d8320d02b241f8
Brandon Runnels <brunnels@uccs.edu>
2021-10-29 11:59:49 -0600

    Removing node_modules and syncing with development

--- node_modules/mkdirp/map.js
+++
@@ -1,6 +0,0 @@
-const {basename} = require('path')
-const map = base =>
-  base === 'index.js' ? 'index.js'
-  : base === 'cmd.js' ? 'bin/cmd.js'
-  : `lib/${base}`
-module.exports = test => map(basename(test))
