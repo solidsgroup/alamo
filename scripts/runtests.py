00de392d54d8307aa3c8434127fd80b8a10a3f8b
Brandon Runnels <brunnels@uccs.edu>
2022-04-07 11:47:48 -0600

    Update to regression tests - eshelby/trig/tension working

--- scripts/runtests.py
+++ scripts/runtests.py
@@ -11,9 +11,18 @@ class color:
     boldyellow   = "\033[1m\033[33m"
     bold = "\033[1m"
     lightgray = "\033[37m"
+    darkgray = "\033[90m"
+
+parser = argparse.ArgumentParser(description='Configure ALAMO');
+parser.add_argument('tests', default=None, nargs='*', help='Spatial dimension [3]')
+parser.add_argument('--serial',action='store_true',default=False,help='Run in serial only (no mpi)')
+args=parser.parse_args()
 
 def test(testdir):
     fails = 0
+    skips = 0
+    tests = 0
+    checks = 0
 
     cfgfile = io.StringIO()
     input = open(testdir + "/input")
@@ -24,65 +33,134 @@ def test(testdir):
     config = configparser.ConfigParser()
     config.read_file(cfgfile)
 
-    print("RUN  {}{}{}".format(color.bold,testdir,color.reset))
+    if not len(config.sections()):
+        print("{}IGNORE {}{}".format(color.darkgray,testdir,color.reset))
+        return 0,0,0,0
+    print("RUN    {}{}{}".format(color.bold,testdir,color.reset))
     for desc in config.sections():
 
-        command = config[desc]['cmd']
+        test  = True
+        check = True
 
-        success=True
-        print("  ├ " + desc)
-        print("  │      Running test..................................................[    ]",end="",flush=True)
-        try:
-            p = subprocess.run(command.split(),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            if p.returncode: raise(Exception("Run failed"))
-        except:
-            print("\b\b\b\b\b\b[{}FAIL{}]".format(color.red,color.reset))
-            fails += 1
+        if 'check' in config[desc].keys(): 
+            if config[desc]['check'] in {"no","No","false","False","0"}:
+                check = False
+            elif config[desc]['check'] in {"yes","Yes","true","True","1"}:
+                check = True
+            else:
+                raise(Exception("Invalid value for check: {}".format(config[desc]['check'])))
+
+        command = ""
+        if 'cmd' in config[desc].keys():
+            command = config[desc]['cmd']
+            if len(config[desc].keys()) > 1:
+                raise Exception("If 'cmd' is specified no other parameters can be set. Received " + ",".join(config[desc].keys))
+        else:
+            dim = 3
+            if 'dim' in config[desc].keys(): dim = int(config[desc]['dim'])
+            nprocs = 1
+            if 'nprocs' in config[desc].keys(): nprocs = int(config[desc]['nprocs'])
+            cmdargs = ""
+            if 'args' in config[desc].keys(): cmdargs = config[desc]['args']
+
+            if nprocs > 1 and args.serial: test = False
+            if nprocs > 1: command += "mpirun -np {} ".format(nprocs)
+            exe = "./bin/alamo-{}d-g++".format(dim)
+            if not os.path.isfile(exe):
+                print("  ├ {}{} (skipped - no executable){}".format(color.boldyellow,desc,color.reset))
+                skips += 1
+                continue
+            command += exe + " "
+            command += "{}/input ".format(testdir)
+            command += cmdargs
+
+        
+        if test:
+            print("  ├ " + desc)
+            print("  ├ " + command)
+            print("  │      Running test............................................",end="",flush=True)
+            try:
+                p = subprocess.check_output(command.split(),stderr=subprocess.PIPE)
+                print("[{}PASS{}]".format(color.boldgreen,color.reset))
+                tests += 1
+            except subprocess.CalledProcessError as e:
+                print("[{}FAIL{}]".format(color.red,color.reset))
+                print("  │      {}CMD   : {}{}".format(color.red,' '.join(e.cmd),color.reset))
+                for line in e.stdout.decode('ascii').split('\n'): print("  │      {}STDOUT: {}{}".format(color.red,line,color.reset))
+                for line in e.stderr.decode('ascii').split('\n'): print("  │      {}STDERR: {}{}".format(color.red,line,color.reset))
+                fails += 1
+                continue
+            except Exception as e:
+                print("[{}FAIL{}]".format(color.red,color.reset))
+                for line in str(e).split('\n'): print("  │      {}{}{}".format(color.red,line,color.reset))
+                continue
+        else:
+            print("  ├ {}{} (skipped - serial only){}".format(color.boldyellow,desc,color.reset))
+            skips += 1
             continue
+            
 
-        print("\b\b\b\b\b\b[{}PASS{}]".format(color.boldgreen,color.reset))
-        print("  │      Checking result...............................................[    ]",end="",flush=True)
-        try:
-            p = subprocess.run(["./test"],cwd=testdir,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-            if p.returncode: raise(Exception("Run failed"))
-        except:
-            print("\b\b\b\b\b\b[{}FAIL{}]".format(color.red,color.reset))
-            fails += 1
-            continue
 
-        print("\b\b\b\b\b\b[{}PASS{}]".format(color.boldgreen,color.reset))
+        if test and check:
+            print("  │      Checking result.........................................",end="",flush=True)
+            try:
+                p = subprocess.check_output(["./test"],cwd=testdir,stderr=subprocess.PIPE)
+                checks += 1
+            except subprocess.CalledProcessError as e:
+                print("[{}FAIL{}]".format(color.red,color.reset))
+                print("  │      {}CMD   : {}{}".format(color.red,' '.join(e.cmd),color.reset))
+                for line in e.stdout.decode('ascii').split('\n'): print("  │      {}STDOUT: {}{}".format(color.red,line,color.reset))
+                for line in e.stderr.decode('ascii').split('\n'): print("  │      {}STDERR: {}{}".format(color.red,line,color.reset))
+                fails += 1
+                continue
+            except Exception as e:
+                print("[{}FAIL{}]".format(color.red,color.reset))
+                for line in str(e).split('\n'): print("  │      {}{}{}".format(color.red,line,color.reset))
+                fails += 1
+                continue
+
+            print("[{}PASS{}]".format(color.boldgreen,color.reset))
         #for line in p.stdout.readlines(): print(line.decode('ascii'),end="")
         #for line in p.stderr.readlines(): print(line.decode('ascii'),end="")
     
-    if fails: print("  └ {}{} tests failed{}".format(color.red,fails,color.reset))
-    else: print("  └ {}{} tests failed{}".format(color.boldgreen,0,color.reset))
-    return fails
+    if fails: print("  └ {}{} tests failed{}".format(color.red,fails,color.reset),end="")
+    else: print("  └ {}{} tests failed{}".format(color.boldgreen,0,color.reset),end="")
+    if skips: print(", {}{} tests skipped{}".format(color.boldyellow,skips,color.reset))
+    else: print("")
+    return fails, checks, tests, skips
 
 
 
 
 
-parser = argparse.ArgumentParser(description='Configure ALAMO');
-parser.add_argument('tests', default=None, nargs='*', help='Spatial dimension [3]')
-args=parser.parse_args()
 
 
 if args.tests: tests = sorted(args.tests)
 else: tests = sorted(glob.glob("./tests/*"))
 
-fails = 0
-skips = 0
+class stats:
+    fails = 0
+    skips = 0
+    checks = 0
+    tests = 0
+
 for testdir in tests:
-    if (not os.path.isdir(testdir)) or (not os.path.isfile(testdir + "/input")) or (not os.path.isfile(testdir + "/test")):
-        print("{}SKIP {}{}".format(color.boldyellow,testdir,color.reset))
-        skips += 1
+    if (not os.path.isdir(testdir)) or (not os.path.isfile(testdir + "/input")):
+        print("{}IGNORE {}{}".format(color.darkgray,testdir,color.reset))
         continue
-    fails += test(testdir)
+    f, c, t, s = test(testdir)
+    stats.fails += f
+    stats.tests += t
+    stats.checks += c
+    stats.skips += s
+    
 
 print("\nTest Summary")
-if not fails: print("{}0 tests failed{}".format(color.boldgreen,color.reset))
-else:         print("{}{} tests failed{}".format(color.red,fails,color.reset))
-if skips: print("{}{} tests skipped{}".format(color.boldyellow,skips,color.reset))
+print("{}{} tests run{}".format(color.green,stats.tests,color.reset))
+print("{}{} tests run and verified{}".format(color.boldgreen,stats.checks,color.reset))
+if not stats.fails: print("{}0 tests failed{}".format(color.boldgreen,color.reset))
+else:         print("{}{} tests failed{}".format(color.red,stats.fails,color.reset))
+if stats.skips: print("{}{} tests skipped{}".format(color.boldyellow,stats.skips,color.reset))
 print("")
 
-exit(fails)
+exit(stats.fails)
