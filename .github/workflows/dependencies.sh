21fdcbbb3c19cb5c7e1302d755bb2effa37e1966
Brandon Runnels <brunnels@uccs.edu>
2022-04-12 07:40:27 -0600

    updated dependencies for documentation

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -8,4 +8,8 @@ sudo apt-get install -y --no-install-recommends \
   libmpich-dev libmpich12 mpich libeigen3-dev \
   python3-pip
 
+# Requirements for regression test scripts
 pip3 install yt matplotlib numpy pandas
+
+# Requirements for building documentation
+pip3 install sphinx breathe m2r sphinx_rtd_theme
