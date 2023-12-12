57cb985933402daef71460110e163ab46ebb89c7
Brandon Runnels <brunnels@uccs.edu>
2023-12-12 10:40:31 -0600

    fixed simple compile error

--- src/alamo.cc
+++ src/alamo.cc
@@ -58,7 +58,7 @@ int main (int argc, char* argv[])
         else if (model == "linear.laplacian")   integrator = new Integrator::Mechanics<Model::Solid::Linear::Laplacian>(pp);
         else if (model == "elastic.neohookean") integrator = new Integrator::Mechanics<Model::Solid::Elastic::NeoHookean>(pp);
         else if (model == "elastic.pseudolinearcubic") integrator = new Integrator::Mechanics<Model::Solid::Elastic::PseudoLinearCubic>(pp);
-        integrator = new Integrator::Mechanics<Model::Solid::Elastic::PseudoLinearCubicPredeformed>(pp);
+        else if (model == "elastic.pseudolinearcubicpredeformed") integrator = new Integrator::Mechanics<Model::Solid::Elastic::PseudoLinearCubicPredeformed>(pp);
         else if (model == "affine.j2")          integrator = new Integrator::Mechanics<Model::Solid::Affine::J2>(pp);
         else Util::Abort(INFO,model," is not a valid model");
     }
