4fae798be90358c0bf8de78f74f2acfb5ae4c50f
Brandon Runnels <brunnels@uccs.edu>
2024-01-24 14:15:32 -0600

    added allen cahn so regtests pass

--- src/alamo.cc
+++ src/alamo.cc
@@ -72,7 +72,7 @@ int main (int argc, char* argv[])
     else if (program == "degradation")          integrator = new Integrator::PolymerDegradation();
     else if (program == "fracture")             integrator = new Integrator::Fracture();
     else if (program == "dendrite")             integrator = new Integrator::Dendrite(pp);
-    //else if (program == "allencahn")            integrator = new Integrator::AllenCahn(pp);
+    else if (program == "allencahn")            integrator = new Integrator::AllenCahn(pp);
     else Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
 
     integrator->InitData();
