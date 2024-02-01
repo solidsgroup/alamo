a3601b9b10e81da6ac4759b07355e1c1aef90bca
meierms1 <meierms@icloud.com>
2024-02-01 18:29:17 -0500

    removed ./inputs

--- inputs/review/.simba/getHash.py
+++
@@ -1,4 +0,0 @@
-
-def getHash(self,directory):
-    print("WARNING: This is the default getHash script!")
-    print("         I cannot do anything!")
--- inputs/review/.simba/parseOutputDir.py
+++
@@ -1,5 +0,0 @@
-
-def parseOutputDir(self,directory):
-    print("WARNING: This is the default parseOutputDir script!")
-    print("         I cannot do anything!")
-    return None
