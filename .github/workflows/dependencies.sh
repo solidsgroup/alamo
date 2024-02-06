a50d71c91a60f3094830c6ae0b6e3324195c59a8
Brandon Runnels <brunnels@iastate.edu>
2024-02-06 08:26:55 -0600

    adding bibtex sphinx extension

--- .github/workflows/dependencies.sh
+++ .github/workflows/dependencies.sh
@@ -12,6 +12,6 @@ sudo apt-get install -y --no-install-recommends \
 pip3 install yt matplotlib numpy pandas
 
 # Requirements for building documentation
-pip3 install sphinx breathe m2r sphinx_rtd_theme linuxdoc sphinx_design sphinx-copybutton
+pip3 install sphinx breathe m2r sphinx_rtd_theme linuxdoc sphinx_design sphinx-copybutton sphinxcontrib-bibtex
 
 npm install -g eclint
