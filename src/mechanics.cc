0c71a3545bcb0a8f2e587ad666be72964a8a6514
Brandon Runnels <brunnels@uccs.edu>
2024-05-06 11:33:17 -0500

    update mechanics

--- src/mechanics.cc
+++ src/mechanics.cc
@@ -8,15 +8,26 @@
 #include "IO/WriteMetaData.H"
 #include "AMReX_ParmParse.H"
 
+#include "Model/Solid/Linear/Isotropic.H"
+#include "Model/Solid/Affine/Isotropic.H"
+#include "Model/Solid/Linear/Cubic.H"
+#include "Model/Solid/Affine/Cubic.H"
+#include "Model/Solid/Finite/NeoHookean.H"
+#include "Model/Solid/Finite/NeoHookeanPredeformed.H"
+#include "Model/Solid/Finite/PseudoLinearCubic.H"
+#include "Model/Solid/Finite/PseudoLinearCubicPredeformed.H"
+#include "Model/Solid/Linear/Laplacian.H"
+#include "Model/Solid/Affine/J2.H"
+#include "Model/Solid/Affine/Hexagonal.H"
+
 #include "Integrator/Mechanics.H"
 #include "Model/Solid/Elastic/CrystalPlastic.H"
-#include "Model/Solid/Elastic/PseudoLinearCubic.H"
 
 int main (int argc, char* argv[])
 {
     Util::Initialize(argc,argv);
 
-    std::string program = "microstructure";
+    std::string program = "mechanics";
     IO::ParmParse pp;
     pp.query("alamo.program",program);
     srand(2);
@@ -26,7 +37,17 @@ int main (int argc, char* argv[])
     {
         std::string model = "linear.isotropic";
         pp.query("alamo.program.mechanics.model",model);
-        if (model == "elastic.pseudolinearcubic")        integrator = new Integrator::Mechanics<Model::Solid::Elastic::PseudoLinearCubic>(pp);
+        if (model == "linear.isotropic")        integrator = new Integrator::Mechanics<Model::Solid::Linear::Isotropic>(pp);
+        else if (model == "linear.cubic")       integrator = new Integrator::Mechanics<Model::Solid::Linear::Cubic>(pp);
+        else if (model == "affine.cubic")       integrator = new Integrator::Mechanics<Model::Solid::Affine::Cubic>(pp);
+        else if (model == "affine.hexagonal")   integrator = new Integrator::Mechanics<Model::Solid::Affine::Hexagonal>(pp);
+        else if (model == "affine.isotropic")   integrator = new Integrator::Mechanics<Model::Solid::Affine::Isotropic>(pp);
+        else if (model == "linear.laplacian")   integrator = new Integrator::Mechanics<Model::Solid::Linear::Laplacian>(pp);
+        else if (model == "finite.neohookean") integrator = new Integrator::Mechanics<Model::Solid::Finite::NeoHookean>(pp);
+        else if (model == "finite.neohookeanpre") integrator = new Integrator::Mechanics<Model::Solid::Finite::NeoHookeanPredeformed>(pp);
+        else if (model == "finite.pseudolinearcubic") integrator = new Integrator::Mechanics<Model::Solid::Finite::PseudoLinearCubic>(pp);
+        else if (model == "finite.pseudolinearcubicpredeformed") integrator = new Integrator::Mechanics<Model::Solid::Finite::PseudoLinearCubicPredeformed>(pp);
+        else if (model == "affine.j2")          integrator = new Integrator::Mechanics<Model::Solid::Affine::J2>(pp);
         else if (model == "elastic.crystalplastic")        integrator = new Integrator::Mechanics<Model::Solid::Elastic::CrystalPlastic>(pp);
         else Util::Abort(INFO,model," is not a valid model");
     }
