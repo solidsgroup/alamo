1ed3fa0780a4b014f8e3fd216fed7d38fa043c12
Brandon Runnels <brunnels@uccs.edu>
2024-06-04 10:06:45 -0600

    This should enable support for PNG on macos

--- .github/workflows/install-macos.sh
+++ .github/workflows/install-macos.sh
@@ -2,21 +2,26 @@ set -eu -o pipefail
 
 #
 # Use Mac OS HomeBrew system to install mpich and eigen
+# [ you should only need to do this once ]
 #
 brew install mpich
 brew install eigen
+brew install libpng
 
 #
 # This command updates the include path to be able to find
 # the above libraries
 #
-export CPLUS_INCLUDE_PATH=$(brew --prefix)/include
+# [ you need to do this in every new shell OR add to your shell config file (like .bashrc) ]
+#
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:$(brew --prefix)/include:$(brew --prefix libpng)/include
 
 #
-# In the alamo directory, run this command with any additiona/
-# arguments. On MacOS, PNG is not supported.
+# In the alamo directory, run this command with any additional arguments. 
 #
-./configure --macos --link=$(brew --prefix)/lib/gcc/current/ 
+# [ you need to include these arguments every time you configure ]
+#
+./configure --macos --link $(brew --prefix)/lib/gcc/current/ $(brew --prefix libpng)/lib/
 
 #
 # Compile the code by running make
@@ -29,7 +34,7 @@ make
 ls ./bin/
 
 #
-# Run the unite test suite
+# Run the unit test suite
 #
 ./bin/test-3d-g++
 
