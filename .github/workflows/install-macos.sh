33b716f611bf41f26753802638e92617ac8cda8c
Brandon Runnels <brunnels@uccs.edu>
2024-06-04 10:38:38 -0600

    final check before merge to development

--- .github/workflows/install-macos.sh
+++ .github/workflows/install-macos.sh
@@ -1,12 +1,22 @@
 set -eu -o pipefail 
 
 #
+# DEPENDENCIES
+# ============
+#
+
+#
 # Use Mac OS HomeBrew system to install mpich and eigen
 # [ you should only need to do this once ]
 #
 brew install mpich
 brew install eigen
 brew install libpng
+
+#
+# CONFIGURATION
+# =============
+#
 
 #
 # This command updates the include path to be able to find
@@ -37,4 +47,22 @@ ls ./bin/
 # Run the unit test suite
 #
 ./bin/test-3d-g++
+
+
+#
+# PYTHON [OPTIONAL]
+# =================
+#
+# These are not required tu run alamo, but are needed to use alamo scripts such as
+# the regression test script.
+# The following commands work on the Github VM, but your configuration may vary.
+#
+# (If you already have another way of installing python packages, use it - the
+# "break-system-packages" is kind of dangerous and is only needed for this CI script)
+#
+
+#
+# Install packages needed for regression test script
+#
+pip3 install sympy yt matplotlib numpy pandas --break-system-packages --user
 
