766b829bb9e290097fcbdcd4192bed905ef0ee3b
Abhijith Anantharanga <abhijeet.ananth@gmail.com>
2024-08-02 14:06:00 -0500

    damage model without using euler angles for grains

---
+++ src/voidpf2.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "Util/Util.H"
+#include "IO/ParmParse.H"
+#include "IO/FileNameParse.H"
+#include "IO/WriteMetaData.H"
+#include "AMReX_ParmParse.H"
+
+#include "Model/Solid/Linear/Isotropic.H"
+#include "Integrator/VoidPF2.H"
+
+int main (int argc, char* argv[])
+{
+    Util::Initialize(argc,argv);
+
+    std::string program = "microstructure";
+    IO::ParmParse pp;
+    pp.query("alamo.program",program);
+    srand(2);
+
+    Integrator::Integrator *integrator = nullptr;
+    if (program == "voidpf2")                integrator = new Integrator::VoidPF2<Model::Solid::Linear::Isotropic>(pp);
+    else Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
+
+    integrator->InitData();
+    integrator->Evolve();
+    delete integrator;
+
+    Util::Finalize();
+} 
