fd29867edb72fa4427bdcb915e01cd5ed3f5d064
Abhijith Thoopul Anantharanga <abhijeet.ananth@gmail.com>
2024-09-20 14:26:52 -0500

    new Tension.H integrator to do tension test on microstructural images

---
+++ src/tension.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "Util/Util.H"
+#include "IO/ParmParse.H"
+#include "IO/FileNameParse.H"
+#include "IO/WriteMetaData.H"
+#include "AMReX_ParmParse.H"
+
+#include "Model/Solid/Linear/Cubic.H"
+#include "Integrator/Tension.H"
+
+int main (int argc, char* argv[])
+{
+    Util::Initialize(argc,argv);
+
+    std::string program = "microstructure";
+    IO::ParmParse pp;
+    pp.query("alamo.program",program);
+    srand(2);
+
+    Integrator::Integrator *integrator = nullptr;
+    if (program == "tension")                integrator = new Integrator::Tension<Model::Solid::Linear::Cubic>(pp);
+    else Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
+
+    integrator->InitData();
+    integrator->Evolve();
+    delete integrator;
+
+    Util::Finalize();
+} 
