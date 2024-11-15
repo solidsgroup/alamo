4839857417fc093ac2b66af9c31915029d1e71b3
Brandon Runnels <brunnels@iastate.edu>
2024-11-15 07:42:54 -0600

    formatted tabs

--- docs/source/Inputs.py
+++ docs/source/Inputs.py
@@ -383,33 +383,108 @@ def scrapeInputsSimple(root="../../src/", writeFiles=True):
             #if "string" in input.keys():
             id = prefix + input["string"]
             if (input["type"] == "select"):
-
-                html += indent + '<div class="card">\n'
-                html += indent + '<div class="card-header">\n'
-                
-                html += indent + '<div class="input-group mb-3">\n'
-                html += indent + '  <div class="input-group-prepend">\n'
-                html += indent + '    <label class="input-group-text" for="{}">{}</label>\n'.format(id+".type",id+".type")
-                html += indent + '  </div>\n'
-                html += indent + '  <select class="custom-select" id="{}" name="{}" onchange="showConditionalInputs(\'{}\')">\n'.format(id+".type",id+".type",id)
+                html     += indent + """<div class="card mb-3" >\n"""
+                html     += indent + """  <div class="card-header">\n"""
+                html     += indent + """    <h5>{}</h5>\n""".format(id + ".type")
+                html     += indent + """    <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">\n"""
                 for cls in input["classes"]:
                     name = cls.split("::")[-1].lower()
-                    html += indent + "<option value='{}'>{}</option>\n".format(name,name)
-                html += "</select>\n"
-                html += indent + '</div>\n'
-
-                html += indent + '</div>\n'
-                html += indent + '<div class="card-body">\n'
-
+                    html += indent + """      <li class="nav-item">\n"""
+                    html += indent + """        <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#{}" role="tab" >{}</a> \n""".format(input["string"] + "." + name, name)
+                    html += indent + """      </li>\n"""
+                html +=     indent + """    </ul>\n"""
+                html +=     indent + """  </div>\n"""
+                html +=     indent + """  <div class="card-body">\n"""
+                html +=     indent + """    <div class="tab-content">\n"""
                 for cls in input["classes"]:
                     name = cls.split("::")[-1].lower()
-                    html += indent + "<div id='{}' class='collapse'>\n".format(id + "." + name)
+                    html += indent + """      <div class="tab-pane" id="{}" role="tabpanel">""".format(input["string"] + "." + name)
                     html += printInputs(cls,indent+"      ",
                                         prefix = input["string"] + "." + name + ".")
-                    html += indent + "</div>"
+                    html += indent + """      </div>\n"""
+                html +=     indent + """    </div>\n"""
+                html +=     indent + """  </div>\n"""
+                html +=     indent + """</div>\n"""
 
-                html += indent + '</div>\n'
-                html += indent + '</div>\n'
+
+
+
+
+#                html += """
+#<div class="card">
+#  <div class="card-header">
+#    <ul class="nav nav-pills card-header-pills">
+#      <li class="nav-item">
+#        <a class="nav-link disabled" href="#">Disabled</a>
+#      </li>
+#      <li class="nav-item"
+#        <div class="dropdown">
+#          <button class="btn btn-secondary dropdown-toggle" type="button" id="{}" data-bs-toggle="dropdown" >
+#<ul class="nav nav-tabs" id="myTab" role="tablist">
+#  <li class="nav-item">
+#    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" >Home</a>
+#  </li>
+#  <li class="nav-item">
+#    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab">Profile</a>
+#  </li>
+#  <li class="nav-item">
+#    <a class="nav-link" id="messages-tab" data-toggle="tab" href="#messages" role="tab">Messages</a>
+#  </li>
+#  <li class="nav-item">
+#    <a class="nav-link" id="settings-tab" data-toggle="tab" href="#settings" role="tab">Settings</a>
+#  </li>
+#</ul>
+#
+#<!-- Tab panes -->
+#<div class="tab-content">
+#  <div class="tab-pane active" id="home" role="tabpanel" >...</div>
+#  <div class="tab-pane" id="profile" role="tabpanel" >...</div>
+#  <div class="tab-pane" id="messages" role="tabpanel" >...</div>
+#  <div class="tab-pane" id="settings" role="tabpanel" >...</div>
+#</div>#            
+#          </button>
+#          <div class="dropdown-menu" >
+#"""
+#                for cls in input["classes"]:
+#                    name = cls.split("::")[-1].lower()
+#                    html += indent + """<a class="dropdown-item" href="#">{}</a>\n""".format(name)
+#
+#                html += """
+#          </div>
+#        </div>
+#      </li>
+#    </ul>
+#
+#  <div class="card-body">
+#
+#
+#""".format(id)
+
+#                html += indent     + '    <div class="input-group mb-3">\n'
+#                html += indent     + '      <div class="input-group-prepend">\n'
+#                html += indent     + '        <label class="input-group-text" for="{}">{}</label>\n'.format(id+".type",id+".type")
+#                html += indent     + '      </div>\n'
+#                html += indent     + '      <select class="dropdown-menu" id="{}" name="{}" onchange="showConditionalInputs(\'{}\')">\n'.format(id+".type",id+".type",id)
+#                for cls in input["classes"]:
+#                    name = cls.split("::")[-1].lower()
+#                    html += indent + "        <option value='{}'>{}</option>\n".format(name,name)
+#                html += indent     + "      </select>\n"
+#                html += indent     + '    </div>\n'
+#                html += indent     + '  </div>\n'
+#                html += indent     + '<div class="card-body">\n'
+#
+#                for cls in input["classes"]:
+#                    name = cls.split("::")[-1].lower()
+#                    html += indent + "<div id='{}'>\n".format(id + "." + name)
+#                    html += printInputs(cls,indent+"      ",
+#                                        prefix = input["string"] + "." + name + ".")
+#                    html += indent + "</div>"
+#
+#                    html += """
+#  </div>
+#</div>
+#"""
+
 
             else:
 
@@ -437,9 +512,21 @@ def scrapeInputsSimple(root="../../src/", writeFiles=True):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dynamic Form with Nested Sections</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+
 </head>
 <body>
+
+
+"""
+    html += "<div class='container mt-5'> <h2 class='mb-4'>Settings Form</h2><form>\n"
+    html += printInputs("Integrator::Flame")
+    html += """
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="main.js"></script>
 
 <script>
     function showConditionalInputs(id) {
@@ -468,15 +555,20 @@ def scrapeInputsSimple(root="../../src/", writeFiles=True):
 
 </script>
 
+
 """
-    html += "<div class='container mt-5'> <h2 class='mb-4'>Settings Form</h2><form>\n"
-    html += printInputs("Integrator::Flame")
-    html += "</form></div>\n"
     
+
+
+
+
     f = open("index.html",'w')
     f.write(html)
     f.close()
     print(html)
+
+
+    
 
 
 
