27a670eff30a9ecf6f502da13147d5d6e0e38dbc
Jacob Fuhrmeister <jfuhrmeister@uiowa.edu>
2025-02-11 21:39:35 -0600

    [SCRUM-04/TASK/57]fix: Edited Riemann2D IC to properly set q1-q4 vectors. Edited Zalesak IC files to properly perform boolean subtraction of rectangular levelset from circle via user input for retangle placement. Need to add fillet to smooth corners. Added fillet_radius temp variable to input.levelset file.

--- src/IC/LS/Zalesak.cpp
+++ src/IC/LS/Zalesak.cpp
@@ -7,46 +7,34 @@
 namespace IC {
 namespace LS {
 
-void Zalesak::Define(Set::Scalar a_radius, Set::Vector a_center, Set::Scalar a_slot_width, Set::Scalar a_slot_length, Type a_type) {
-    radius = a_radius;
-    center = a_center;
-    type = a_type;
-    slot_width = (a_slot_width > 0) ? a_slot_width : 0.1 * radius;
-    slot_length = (a_slot_length > 0) ? a_slot_length : 0.6 * radius;
+Zalesak::Zalesak(amrex::Vector<amrex::Geometry>& _geom) : IC(_geom) {}
+
+Zalesak::Zalesak(amrex::Vector<amrex::Geometry>& _geom, IO::ParmParse& pp) : IC(_geom) {
+    pp_queryclass(*this);
+}
+
+Zalesak::Zalesak(amrex::Vector<amrex::Geometry>& _geom, IO::ParmParse& pp, const std::string& name) : IC(_geom) {
+    pp_queryclass(name, *this);
 }
 
 void Zalesak::Add(const int& lev, Set::Field<Set::Scalar>& a_field, Set::Scalar) {
     bool cellcentered = (a_field[0]->boxArray().ixType() == amrex::IndexType(amrex::IntVect::TheCellVector()));
     const Set::Scalar* DX = geom[lev].CellSize();
-    
+    const Set::Scalar Narrow_Band_Width = 6.0 * DX[0];
+    const Set::Scalar InnerTube = -Narrow_Band_Width;
+    const Set::Scalar OuterTube = Narrow_Band_Width;
+
     for (amrex::MFIter mfi(*a_field[lev], amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi) {
         amrex::Box bx = mfi.tilebox();
-        amrex::Array4<Set::Scalar> const& field = a_field[lev]->array(mfi);
-        
-        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
-            Set::Scalar x, y, z;
-            if (cellcentered) {
-                x = geom[lev].ProbLo()[0] + ((amrex::Real)(i) + 0.5) * DX[0];
-                y = geom[lev].ProbLo()[1] + ((amrex::Real)(j) + 0.5) * DX[1];
-                z = geom[lev].ProbLo()[2] + ((amrex::Real)(k) + 0.5) * DX[2];
-            } else {
-                x = geom[lev].ProbLo()[0] + (amrex::Real)(i) * DX[0];
-                y = geom[lev].ProbLo()[1] + (amrex::Real)(j) * DX[1];
-                z = geom[lev].ProbLo()[2] + (amrex::Real)(k) * DX[2];
-            }
+        auto field = a_field[lev]->array(mfi);
 
-            Set::Scalar rsq = (type == Type::XYZ) ? (x - center(0)) * (x - center(0)) + (y - center(1)) * (y - center(1)) + (z - center(2)) * (z - center(2))
-                         : (type == Type::XY)  ? (x - center(0)) * (x - center(0)) + (y - center(1)) * (y - center(1))
-                         : (type == Type::YZ)  ? (y - center(1)) * (y - center(1)) + (z - center(2)) * (z - center(2))
-                         : (x - center(0)) * (x - center(0)) + (z - center(2)) * (z - center(2));
-            
+        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+            auto coords = computeCoordinates(i, j, k, lev, cellcentered);
+            Set::Scalar rsq = computeRSquared(coords, type, center);
             Set::Scalar distance = std::sqrt(rsq) - radius;
-            if (x > center(0) - 0.5 * slot_width && x < center(0) + 0.5 * slot_width &&
-                y < center(1) + radius && y > center(1) + radius - slot_length) {
-                field(i, j, k, 0) = 1.0;
-            } else {
-                field(i, j, k, 0) = distance;
-            }
+            Set::Scalar slot_ls = computeSlotLevelSet(coords);
+            Set::Scalar combined_distance = std::max(distance, -slot_ls); // Ensure subtraction
+            field(i, j, k, 0) = computeLevelSetValue(combined_distance, Narrow_Band_Width, InnerTube, OuterTube);
         });
     }
 }
@@ -54,14 +42,87 @@ void Zalesak::Add(const int& lev, Set::Field<Set::Scalar>& a_field, Set::Scalar)
 void Zalesak::Parse(Zalesak& value, IO::ParmParse& pp) {
     pp_query("radius", value.radius);
     pp_queryarr("center", value.center);
-    pp_query("slot_width", value.slot_width);
-    pp_query("slot_length", value.slot_length);
-    std::string type;
-    pp_query("type", type);
-    if (type == "yz" || type == "zy")      value.type = Type::YZ;
-    else if (type == "zx" || type == "xz") value.type = Type::ZX;
-    else if (type == "xy" || type == "yx") value.type = Type::XY;
-    else                                   value.type = Type::XYZ;
+    pp_queryarr("slot_center", value.slot_center);
+    pp_queryarr("slot_size", value.slot_size);
+    pp_query("fillet_radius", value.fillet_radius);
+    std::string type_str;
+    pp_query("type", type_str);
+
+    if (type_str == "yz" || type_str == "zy") value.type = Type::YZ;
+    else if (type_str == "zx" || type_str == "xz") value.type = Type::ZX;
+    else if (type_str == "xy" || type_str == "yx") value.type = Type::XY;
+    else value.type = Type::XYZ;
+}
+
+// Compute physical coordinates of the grid cell
+AMREX_GPU_HOST_DEVICE Set::Vector Zalesak::computeCoordinates(int i, int j, int k, int lev, bool cellcentered) const {
+    Set::Vector coords;
+    const auto& DX = geom[lev].CellSize();
+    const auto& prob_lo = geom[lev].ProbLo();
+
+    if (cellcentered) {
+        AMREX_D_TERM(coords(0) = prob_lo[0] + (i + 0.5) * DX[0];,
+                     coords(1) = prob_lo[1] + (j + 0.5) * DX[1];,
+                     coords(2) = prob_lo[2] + (k + 0.5) * DX[2];);
+    } else {
+        AMREX_D_TERM(coords(0) = prob_lo[0] + i * DX[0];,
+                     coords(1) = prob_lo[1] + j * DX[1];,
+                     coords(2) = prob_lo[2] + k * DX[2];);
+    }
+    return coords;
+}
+
+Set::Scalar Zalesak::computeRSquared(const Set::Vector& coords, Type type, const Set::Vector& center) const {
+    switch (type) {
+    case Type::XYZ:
+        return AMREX_D_TERM((coords(0) - center(0)) * (coords(0) - center(0)),
+                            + (coords(1) - center(1)) * (coords(1) - center(1)),
+                            + (coords(2) - center(2)) * (coords(2) - center(2)));
+    case Type::XY:
+        return (coords(0) - center(0)) * (coords(0) - center(0)) +
+               (coords(1) - center(1)) * (coords(1) - center(1));
+    case Type::YZ:
+        return (coords(1) - center(1)) * (coords(1) - center(1)) +
+               (coords(2) - center(2)) * (coords(2) - center(2));
+    case Type::ZX:
+        return (coords(0) - center(0)) * (coords(0) - center(0)) +
+               (coords(2) - center(2)) * (coords(2) - center(2));
+    default:
+        return NAN;
+    }
+}
+
+Set::Scalar Zalesak::computeSlotLevelSet(const Set::Vector& coords) const {
+    Set::Vector d = (coords - slot_center).cwiseAbs() - slot_size / 2.0;
+    Set::Vector d_clamped = d.cwiseMax(Set::Vector::Zero());
+    Set::Scalar outside_distance = d_clamped.norm();
+    Set::Scalar inside_distance = std::min(std::max(d(0), std::max(d(1), d(2))), 0.0);
+    return outside_distance + inside_distance;
+}
+
+/*Set::Scalar Zalesak::computeSlotLevelSet(const Set::Vector& coords) const {
+    // Compute distance from rectangle edges
+    Set::Vector d = (coords - slot_center).cwiseAbs() - slot_size / 2.0;
+    
+    // If inside the rectangle, return negative distance
+    if (d(0) <= 0 && d(1) <= 0) {
+        return -std::sqrt(d.squaredNorm());
+    }
+
+    // Handle fillet at the corners
+    Set::Scalar corner_dist = std::sqrt(std::max(d(0), 0.0) * std::max(d(0), 0.0) +
+                                        std::max(d(1), 0.0) * std::max(d(1), 0.0));
+
+    // Apply fillet smoothing
+    //std::cout << "fillet_radius:" << " " << fillet_radius << std::endl;
+    return (corner_dist - fillet_radius);
+}*/
+
+Set::Scalar Zalesak::computeLevelSetValue(Set::Scalar distance, Set::Scalar Narrow_Band_Width, Set::Scalar InnerTube, Set::Scalar OuterTube) const {
+    if (std::abs(distance) <= Narrow_Band_Width) return distance;
+    if (distance < -Narrow_Band_Width) return InnerTube;
+    if (distance > Narrow_Band_Width) return OuterTube;
+    return NAN;
 }
 
 } // namespace LS
