20757ac85f94e7e38588a1704abe8afe76539cdb
Eric Sandall <esandall@solids.group>
2025-03-06 11:29:56 -0600

    Initial commit for eos development with Cantera

--- src/cantera.cc
+++ src/cantera.cc
@@ -1,5 +1,12 @@
 #ifdef ALAMO_CANTERA
 #include "cantera/core.h"
+#include "cantera/base/AnyMap.h"
+#include <cantera/zeroD/IdealGasReactor.h>
+#include <cantera/zeroD/ReactorNet.h>
+#include <cantera/thermo/IdealGasPhase.h>
+#include <cantera/kinetics/InterfaceKinetics.h>
+#include <cantera/kinetics/Kinetics.h>
+#include <cantera/base/Solution.h>
 #include <iostream>
 #endif
 
@@ -8,14 +15,32 @@
 void simple_demo()
 {
     // Create a new Solution object
-  auto sol = Cantera::newSolution("h2o2.yaml");
+  auto sol = Cantera::newSolution("gri30.yaml", "gri30", "None");
     auto gas = sol->thermo();
 
     // Set the thermodynamic state by specifying T (500 K) P (2 atm) and the mole
     // fractions. Note that the mole fractions do not need to sum to 1.0 - they will
     // be normalized internally. Also, the values for any unspecified species will be
     // set to zero.
-    gas->setState_TPX(500.0, 2.0*OneAtm, "H2O:1.0, H2:8.0, AR:1.0");
+    Cantera::AnyMap state;
+    state["U"] = 100000.0;
+    state["V"] = 1.0e-3;
+    state["Y"] = "H2:2.0, O2:2.0";
+    
+    gas->setState(state);
+    std::cout << gas->report() << std::endl;
+
+    Cantera::IdealGasReactor reactor;
+    reactor.setSolution(sol);
+    Cantera::ReactorNet reactor_net;
+    reactor_net.addReactor(reactor);
+    double dt = 1e2;
+    double t = 0.0;
+    double tend = 1e8;
+    while (t < tend) {
+      t = reactor_net.step();
+    }
+    //gas->setState_TRX(500.0, 1.0*Cantera::OneAtm, "H2O:1.0, H2:8.0, AR:1.0");
 
     // Print a summary report of the state of the gas.
     std::cout << gas->report() << std::endl;
@@ -30,7 +55,7 @@ int main()
 #ifdef ALAMO_CANTERA
     try {
         simple_demo();
-    } catch (CanteraError& err) {
+    } catch (Cantera::CanteraError& err) {
         std::cout << err.what() << std::endl;
         return 1;
     }
