2e5d8cae164b20052e9c8d4bed4351fec6a135d0
Brandon Runnels <brunnels@iastate.edu>
2025-12-11 13:09:59 -0600

    Updating python script to include directions to build alamo py

--- .github/workflows/python.sh
+++ .github/workflows/python.sh
@@ -1,10 +1,15 @@
 set -eu -o pipefail 
 
+# Must use debug mode using g++ for python
+./configure --comp=g++ --debug
+
+# Build python package
+make py
+
 # Set up a virtual environment and activate
 mkdir alamovenv 
 python -m venv alamovenv
 source alamovenv/bin/activate
-
 
 # Install alamo using pip (-e for editable mode is optional)
 pip install -e .
