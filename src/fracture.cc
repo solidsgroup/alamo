86af4d6e9b51979da2efa1b372e604a10d7f476d
Vinamra Agrawal <vagrawal@lanl.gov>
2026-02-18 13:28:56 -0700

    added ductile fracture integrator. need to merge it with brittle at some point

--- src/fracture.cc
+++ src/fracture.cc
@@ -10,6 +10,7 @@
 #include "Util/Util.H"
 #include "IO/ParmParse.H"
 #include "Integrator/Fracture.H"
+#include "Integrator/DuctileFracture.H"
 
 int main (int argc, char* argv[])
 {
@@ -19,11 +20,12 @@ int main (int argc, char* argv[])
     IO::ParmParse pp;
     // This input determines which integrator is used.
     pp.query_validate(  "alamo.program", program,
-                        {"fracture"});
+                        {"fracture", "ductilefracture"});
     srand(2);
 
     Integrator::Integrator *integrator = nullptr;
     if       (program == "fracture")             pp.select_only<Integrator::Fracture>(integrator);
+    if       (program == "ductilefracture")      pp.select_only<Integrator::DuctileFracture>(integrator);
     else Util::Abort(INFO,"Error: \"",program,"\" is not a valid program.");
 
     integrator->InitData();
