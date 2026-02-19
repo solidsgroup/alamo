5fbff22275bd58fded93f71addf696805865bbb9
Caleb Munger <mungerct@iastate.edu>
2026-02-19 15:51:50 -0600

    new imput files

--- alamo.sh
+++ alamo.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#SBATCH --time=16:00:00
+#SBATCH --time=24:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=32
+#SBATCH --ntasks-per-node=128
 #SBATCH --mem-per-cpu=15000
-#SBATCH --job-name="alamo"
+#SBATCH --job-name="strand_m3"
 #SBATCH --output="%x-%j-log.txt"
 #SBATCH --mail-user=mungerct@iastate.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -19,4 +19,5 @@ module load openmpi_hpc
 # srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_kodga_validation phi.ic.psread.file.name="setC_xyzrs/uni_R240um_AP6499_10.xyzr" plot_file="output.scpsphereselastic_set10_phi_eps_8"
 # srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_kodga_validation
 # srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_arbitary_geometry
-srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_voidmeshconverge
+# srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_voidmeshconverge
+srun --mpi=pmix ./bin/alamo-2d-g++ input.scpspheres_strand_burn
