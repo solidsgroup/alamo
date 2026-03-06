ccb5d8bdd9ffe155aa6f37645731771c6f139464
Eric Sandall <esandall@solids.group>
2026-03-06 14:10:07 -0600

    Added diffuse pressure correction to Roe.H

--- src/Integrator/Hydro.cpp
+++ src/Integrator/Hydro.cpp
@@ -10,7 +10,7 @@
 #include "IC/Expression.H"
 #include "IC/BMP.H"
 #include "IC/PNG.H"
-//#include "Solver/Local/Riemann/Roe.H"
+#include "Solver/Local/Riemann/Roe.H"
 //#include "Solver/Local/Riemann/HLLE.H"
 #include "Solver/Local/Riemann/HLLC.H"
 #include "AMReX_TimeIntegrator.H"
@@ -182,7 +182,7 @@ Hydro::Parse(Hydro& value, IO::ParmParse& pp)
     pp.select_default<IC::Constant,IC::Expression>("q.ic",value.ic_q,value.geom);
 
     // Riemann solver
-    pp.select_default<  //Solver::Local::Riemann::Roe,
+    pp.select_default<  Solver::Local::Riemann::Roe,
                         //Solver::Local::Riemann::HLLE,
                         Solver::Local::Riemann::HLLC>("solver",value.riemannsolver);
 
