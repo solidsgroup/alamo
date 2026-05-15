d4784cad047d9c9f328fa7bb6706da4efcb99ced
Matthew Mehrtens <12023414+mcmehrtens@users.noreply.github.com>
2026-05-15 15:42:43 -0500

    Correct DDCH implementation derivation

--- src/Integrator/DoublyDegenerateCahnHilliard.cpp
+++ src/Integrator/DoublyDegenerateCahnHilliard.cpp
@@ -43,7 +43,8 @@ void DoublyDegenerateCahnHilliard::Parse(DoublyDegenerateCahnHilliard &value, IO
     value.RegisterNewFab(value.etanew_mf, value.bc, 1, 1, "eta", true);
     value.RegisterNewFab(value.etaold_mf, value.bc, 1, 1, "eta_old", false);
     value.RegisterNewFab(value.deta_mf, value.bc, 1, 1, "deta", true);
-    value.RegisterNewFab(value.driving_force_mf, value.bc, 1, 1, "driving_Force", true, false);
+    value.RegisterNewFab(value.driving_force_mf, value.bc, 1, 1, "driving_force", true, false);
+    value.AddField<Set::Vector, Set::Hypercube::Cell>(value.evolution_driving_force_mf, nullptr, 1, 1, "evolution_driving_force", true, false);
 
     value.RegisterIntegratedVariable(&value.volume, "volume");
     value.integrate_variables_before_advance = false;
@@ -91,9 +92,9 @@ DoublyDegenerateCahnHilliard::Advance(int lev, Set::Scalar /*time*/, Set::Scalar
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
             Set::Scalar eta = etaold(i, j, k);
-            Set::Scalar g_alpha = 1.0 / sqrt(gamma * gamma * (eta * eta * (1 - eta) * (1 - eta)) + alpha * alpha * epsilon * epsilon);
-            Set::Scalar g_alphaprime_denominator = sqrt(eta * eta * (1 - eta) * (1 - eta) + alpha * alpha * epsilon * epsilon);
-            Set::Scalar g_alphaprime = eta * (1 - eta) * (2 * eta - 1) / (gamma * g_alphaprime_denominator * g_alphaprime_denominator * g_alphaprime_denominator);
+            Set::Scalar g_alpha_denominator = sqrt(gamma * gamma * (1 - eta) * (1 - eta) * eta * eta + alpha * alpha * epsilon * epsilon);
+            Set::Scalar g_alpha = 1.0 / g_alpha_denominator;
+            Set::Scalar g_alphaprime = -gamma * gamma * (eta - 1) * eta * (2 * eta - 1) / (g_alpha_denominator * g_alpha_denominator * g_alpha_denominator);
             Set::Scalar f = omega / 4 * eta * eta * (1 - eta) * (1 - eta);
             Set::Scalar fprime = omega / 2 * eta * (1 - eta) * (1 - 2 * eta);
             Set::Vector grad_eta = Numeric::Gradient(etaold, i, j, k, 0, DX);
@@ -106,26 +107,62 @@ DoublyDegenerateCahnHilliard::Advance(int lev, Set::Scalar /*time*/, Set::Scalar
 
     driving_force_mf[lev]->FillBoundary(geom[lev].periodicity());
 
+    // Uncomment the next two `for` loops to use the non-product rule formulation
+    //
     for (amrex::MFIter mfi(*etanew_mf[lev], true); mfi.isValid(); ++mfi)
     {
         const amrex::Box &bx = mfi.tilebox();
-        Set::Patch<Set::Scalar> &etanew = etanew_mf[lev]->array(mfi);
         Set::Patch<const Set::Scalar> &etaold = etaold_mf[lev]->array(mfi);
         Set::Patch<const Set::Scalar> &driving_force = driving_force_mf[lev]->array(mfi);
-        Set::Patch<Set::Scalar> &deta = deta_mf[lev]->array(mfi);
+        Set::Patch<Set::Vector> evolution_driving_force = evolution_driving_force_mf[lev]->array(mfi);
 
         amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
             Set::Scalar eta = etaold(i, j, k);
-            Set::Vector grad_eta = Numeric::Gradient(etaold, i, j, k, 0, DX);
             Set::Scalar M_alpha = mu * eta * eta * (1 - eta) * (1 - eta) + alpha * epsilon;
-            Set::Scalar M_alphaprime = 2.0 * mu * eta * (eta - 1) * (2 * eta - 1);
             Set::Vector grad_driving_force = Numeric::Gradient(driving_force, i, j, k, 0, DX);
-            Set::Scalar lap_driving_force = Numeric::Laplacian(driving_force, i, j, k, 0, DX);
 
-            etanew(i, j, k) = 1.0 / epsilon * (M_alphaprime * grad_eta.dot(grad_driving_force) + M_alpha * lap_driving_force) * dt + etaold(i, j, k);
+            evolution_driving_force(i, j, k) = M_alpha * grad_driving_force;
+        });
+    }
+
+    evolution_driving_force_mf[lev]->FillBoundary(geom[lev].periodicity());
+
+    for (amrex::MFIter mfi(*etanew_mf[lev], true); mfi.isValid(); ++mfi)
+    {
+        const amrex::Box &bx = mfi.tilebox();
+        Set::Patch<const Set::Vector> evolution_driving_force = evolution_driving_force_mf[lev]->array(mfi);
+        Set::Patch<Set::Scalar> &etanew = etanew_mf[lev]->array(mfi);
+        Set::Patch<const Set::Scalar> &etaold = etaold_mf[lev]->array(mfi);
+        Set::Patch<Set::Scalar> &deta = deta_mf[lev]->array(mfi);
+
+        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+            etanew(i, j, k) = etaold(i, j, k) + dt / epsilon * Numeric::Divergence(evolution_driving_force, i, j, k, DX);
             deta(i, j, k) = etanew(i, j, k) - etaold(i, j, k);
         });
     }
+
+    // Uncomment the next `for` loop to use the product rule formulation
+    //
+    // for (amrex::MFIter mfi(*etanew_mf[lev], true); mfi.isValid(); ++mfi)
+    // {
+    //     const amrex::Box &bx = mfi.tilebox();
+    //     Set::Patch<Set::Scalar> &etanew = etanew_mf[lev]->array(mfi);
+    //     Set::Patch<const Set::Scalar> &etaold = etaold_mf[lev]->array(mfi);
+    //     Set::Patch<const Set::Scalar> &driving_force = driving_force_mf[lev]->array(mfi);
+    //     Set::Patch<Set::Scalar> &deta = deta_mf[lev]->array(mfi);
+
+    //     amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+    //         Set::Scalar eta = etaold(i, j, k);
+    //         Set::Vector grad_eta = Numeric::Gradient(etaold, i, j, k, 0, DX);
+    //         Set::Scalar M_alpha = mu * eta * eta * (1 - eta) * (1 - eta) + alpha * epsilon;
+    //         Set::Scalar M_alphaprime = 2.0 * mu * eta * (eta - 1) * (2 * eta - 1);
+    //         Set::Vector grad_driving_force = Numeric::Gradient(driving_force, i, j, k, 0, DX);
+    //         Set::Scalar lap_driving_force = Numeric::Laplacian(driving_force, i, j, k, 0, DX);
+
+    //         etanew(i, j, k) = etaold(i, j, k) + dt / epsilon * (M_alphaprime * grad_eta.dot(grad_driving_force) + M_alpha * lap_driving_force);
+    //         deta(i, j, k) = etanew(i, j, k) - etaold(i, j, k);
+    //     });
+    // }
 }
 
 void
