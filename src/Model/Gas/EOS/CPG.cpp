3d382a5655c715c7483a15d53c3f8e5c908330af
Brandon Runnels <brunnels@iastate.edu>
2026-06-19 16:40:07 -0500

    Updating Model::Gas family of classes to make GPU safe

--- src/Model/Gas/EOS/CPG.cpp
+++ src/Model/Gas/EOS/CPG.cpp
@@ -6,31 +6,31 @@ namespace Gas {
 namespace EOS {
 
 double CPG::ComputeT(double density, double momentumx, double momentumy, double E, double Tguess,
-                Set::Patch<const Set::Scalar>& X, int i, int j, int k, double /*rtol=1e-12*/) const {
+                double R, double gamma, double /*rtol=1e-12*/) const {
     // Temperature, K
     // Since gamma is not a function of temperature, but is a function of composition, we can
     // compute gamma at any dummy temperature
-    double P = (E - 0.5*(momentumx*momentumx + momentumy*momentumy)/density) * (gas->gamma(Tguess, X, i, j, k) - 1.0);
-    double T = P / density / gas->R(X, i, j, k);
+    double P = (E - 0.5*(momentumx*momentumx + momentumy*momentumy)/density) * (gamma - 1.0);
+    double T = P / density / R;
     return T;
 }
 double CPG::ComputeT(double pressure, double density,
-                Set::Patch<const Set::Scalar>& X, int i, int j, int k) const {
+                double R) const {
     // Temperature, K
-    double T = pressure / density / gas->R(X, i, j, k);
+    double T = pressure / density / R;
     return T;
 }
 double CPG::ComputeP(double density, double T,
-                Set::Patch<const Set::Scalar>& X, int i, int j, int k) const {
+                double R) const {
     // Pressure, Pa
-    double P = density * gas->R(X, i, j, k) * T;
+    double P = density * R * T;
     return P;
 }
 double CPG::ComputeE(double density, double momentumx, double momentumy, double T,
-                Set::Patch<const Set::Scalar>& X, int i, int j, int k) const {
+                double R, double gamma) const {
     // Energy, J/m^3
-    double P = density * gas->R(X, i, j, k) * T;
-    double rhoE = P / (gas->gamma(T, X, i, j, k) - 1.0);
+    double P = density * R * T;
+    double rhoE = P / (gamma - 1.0);
     double E = rhoE + 0.5*(momentumx*momentumx + momentumy*momentumy)/density;
     return E;
 }
