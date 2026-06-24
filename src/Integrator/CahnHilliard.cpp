dcb037f1a5a03022512b01db97f4ab103abd2e68
Brandon Runnels <Brandon Runnels>
2026-06-24 09:41:15 -0700

    fft appears to work now

--- src/Integrator/CahnHilliard.cpp
+++ src/Integrator/CahnHilliard.cpp
@@ -1,6 +1,7 @@
 #include "IC/Expression.H"
 #include <AMReX_MLPoisson.H>
 #include <algorithm>
+#include <cmath>
 #include <limits>
 
 #ifdef ALAMO_FFT
@@ -33,6 +34,7 @@ void CahnHilliard::Parse(CahnHilliard &value, IO::ParmParse &pp)
     pp.query_default("L",    value.L,     1.0);
     pp.query_validate("mobility", value.mobility, {"constant", "singly_degenerate"});
     pp.query_default("mobility_floor", value.mobility_floor, 0.0);
+    pp.query_default("spectral_stabilization", value.spectral_stabilization, std::numeric_limits<Set::Scalar>::quiet_NaN());
     // Regridding criterion
     pp.query_default("refinement_threshold",value.refinement_threshold, 1E100);
 
@@ -129,9 +131,8 @@ CahnHilliard::AdvanceReal (int lev, Set::Scalar time, Set::Scalar dt)
 void
 CahnHilliard::AdvanceSpectral (int lev, Set::Scalar /*time*/, Set::Scalar dt)
 {
-    //
-    // FFT Boilerplate
-    //
+    Util::Assert(INFO, TEST(lev == 0), "CahnHilliard spectral method currently supports one AMR level");
+
     amrex::FFT::R2C my_fft(this->geom[lev].Domain());
     auto const &[cba, cdm] = my_fft.getSpectralDataLayout();
     const Set::Scalar* DX = geom[lev].CellSize();
@@ -142,76 +143,137 @@ CahnHilliard::AdvanceSpectral (int lev, Set::Scalar /*time*/, Set::Scalar dt)
             pi_Ly = 2.0 * Set::Constant::Pi / geom[lev].Domain().length(1) / DX[1],
             pi_Lz = 2.0 * Set::Constant::Pi / geom[lev].Domain().length(2) / DX[2]);
     Set::Scalar scaling = 1.0 / geom[lev].Domain().d_numPts();
-    
 
-    //
-    // Compute the gradient of the chemical potential in realspace
-    //
-    for ( amrex::MFIter mfi(*etanew_mf[lev],true); mfi.isValid(); ++mfi )
+    etanew_mf[lev]->FillBoundary(geom[lev].periodicity());
+
+    if (mobility == "constant")
     {
-        const amrex::Box& bx = mfi.tilebox();
-        amrex::Array4<const amrex::Real> const& eta = etanew_mf[lev]->array(mfi);
-        amrex::Array4<amrex::Real> const& inter    = intermediate[lev]->array(mfi);
-        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+        for ( amrex::MFIter mfi(*etanew_mf[lev],true); mfi.isValid(); ++mfi )
         {
-            inter(i,j,k) = eta(i,j,k)*eta(i,j,k)*eta(i,j,k) - eta(i,j,k);
-        });
+            const amrex::Box& bx = mfi.tilebox();
+            amrex::Array4<const amrex::Real> const& eta = etanew_mf[lev]->array(mfi);
+            amrex::Array4<amrex::Real> const& inter    = intermediate[lev]->array(mfi);
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+            {
+                inter(i,j,k) = eta(i,j,k)*eta(i,j,k)*eta(i,j,k) - eta(i,j,k);
+            });
+        }
+
+        intermediate[lev]->FillBoundary(geom[lev].periodicity());
+
+        amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > eta_hat_mf(cba, cdm, 1, 0);
+        amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > chempot_hat_mf(cba, cdm, 1, 0);
+        my_fft.forward(*etanew_mf[lev], eta_hat_mf);
+        my_fft.forward(*intermediate[lev], chempot_hat_mf);
+
+        for (amrex::MFIter mfi(eta_hat_mf, false); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box &bx = mfi.tilebox();
+            amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & eta_hat = eta_hat_mf.array(mfi);
+            amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & chempot_hat = chempot_hat_mf.array(mfi);
+
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int m, int n, int p) {
+                AMREX_D_TERM(
+                    Set::Scalar k1 = m * pi_Lx;,
+                    Set::Scalar k2 = (n < domain.length(1)/2 ? n * pi_Ly : (n - domain.length(1)) * pi_Ly);,
+                    Set::Scalar k3 = (p < domain.length(2)/2 ? p * pi_Lz : (p - domain.length(2)) * pi_Lz););
+
+                Set::Scalar omega2 = AMREX_D_TERM(k1 * k1, + k2 * k2, + k3*k3);
+                Set::Scalar omega4 = omega2 * omega2;
+
+                eta_hat(m, n, p) =
+                    (eta_hat(m, n, p) - L * omega2 * chempot_hat(m, n, p) * dt) /
+                    (1.0 + L * gamma * omega4 * dt);
+                eta_hat(m,n,p) *= scaling;
+            });
+        }
+
+        my_fft.backward(eta_hat_mf, *etanew_mf[lev]);
     }
-
-    intermediate[lev]->FillBoundary();
-
-    //
-    // FFT of eta
-    // 
-    amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > eta_hat_mf(cba, cdm, 1, 0);
-    my_fft.forward(*etanew_mf[lev], eta_hat_mf);
-
-    //
-    // FFT of chemical potential gradient
-    //
-    amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > chempot_hat_mf(cba, cdm, 1, 0);
-    my_fft.forward(*intermediate[lev], chempot_hat_mf);
-
-    //
-    // Perform update in spectral coordinatees
-    //
-    //for (amrex::MFIter mfi(eta_hat_mf, amrex::TilingIfNotGPU()); mfi.isValid(); ++mfi)
-    for (amrex::MFIter mfi(eta_hat_mf, false); mfi.isValid(); ++mfi)
+    else if (mobility == "singly_degenerate")
     {
-        const amrex::Box &bx = mfi.tilebox();
+        for (amrex::MFIter mfi(*etanew_mf[lev], true); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box& bx = mfi.tilebox();
+            amrex::Array4<const amrex::Real> const& eta = etanew_mf[lev]->array(mfi);
+            amrex::Array4<amrex::Real> const& inter = intermediate[lev]->array(mfi);
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+            {
+                Set::Scalar lap_eta = Numeric::Laplacian(eta, i, j, k, 0, DX);
+                inter(i,j,k) = eta(i,j,k)*eta(i,j,k)*eta(i,j,k) - eta(i,j,k) - gamma*lap_eta;
+            });
+        }
 
-        Util::Message(INFO, bx);
-        
-        amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & eta_hat     =  eta_hat_mf.array(mfi);
-        amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & chempot_hat =  chempot_hat_mf.array(mfi);
+        intermediate[lev]->FillBoundary(geom[lev].periodicity());
 
-        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int m, int n, int p) {
+        amrex::MultiFab flux_mf(etanew_mf[lev]->boxArray(), etanew_mf[lev]->DistributionMap(), AMREX_SPACEDIM, 0);
+        for (amrex::MFIter mfi(*etanew_mf[lev], true); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box& bx = mfi.tilebox();
+            amrex::Array4<const amrex::Real> const& eta = etanew_mf[lev]->array(mfi);
+            amrex::Array4<const amrex::Real> const& inter = intermediate[lev]->array(mfi);
+            amrex::Array4<amrex::Real> const& flux = flux_mf.array(mfi);
+            const Set::Scalar L_local = L;
+            const Set::Scalar M_floor = mobility_floor;
 
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int i, int j, int k)
+            {
+                Set::Scalar alpha = 0.5 * (eta(i,j,k) + 1.0);
+                Set::Scalar M = L_local * alpha * alpha * (1.0 - alpha) * (1.0 - alpha) + M_floor;
+                Set::Vector grad_inter = Numeric::Gradient(inter, i, j, k, 0, DX);
+                AMREX_D_TERM(
+                    flux(i,j,k,0) = M * grad_inter(0);,
+                    flux(i,j,k,1) = M * grad_inter(1);,
+                    flux(i,j,k,2) = M * grad_inter(2););
+            });
+        }
 
-            // Get spectral coordinates
-            AMREX_D_TERM(
-                Set::Scalar k1 = m * pi_Lx;,
-                Set::Scalar k2 = (n < domain.length(1)/2 ? n * pi_Ly : (n - domain.length(1)) * pi_Ly);,
-                Set::Scalar k3 = (p < domain.length(2)/2 ? p * pi_Lz : (p - domain.length(2)) * pi_Lz););
+        flux_mf.FillBoundary(geom[lev].periodicity());
 
+        amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > eta_hat_mf(cba, cdm, 1, 0);
+        amrex::FabArray<amrex::BaseFab<amrex::GpuComplex<Set::Scalar> > > flux_hat_mf(cba, cdm, AMREX_SPACEDIM, 0);
+        my_fft.forward(*etanew_mf[lev], eta_hat_mf);
+        AMREX_D_TERM(
+            my_fft.forward(flux_mf, flux_hat_mf, 0, 0);,
+            my_fft.forward(flux_mf, flux_hat_mf, 1, 1);,
+            my_fft.forward(flux_mf, flux_hat_mf, 2, 2););
 
-            Set::Scalar lap = AMREX_D_TERM(k1 * k1, + k2 * k2, + k3*k3);
-                        
-            Set::Scalar bilap = lap*lap;
+        Set::Scalar S = spectral_stabilization;
+        if (std::isnan(S))
+            S = gamma * (L * 0.0625 + mobility_floor);
+        amrex::GpuComplex<Set::Scalar> I(0.0, 1.0);
 
-            eta_hat(m, n, p) =
-                (eta_hat(m, n, p) - L * lap * chempot_hat(m, n, p) * dt) /
-                (1.0 + L * gamma * bilap * dt);
-                
+        for (amrex::MFIter mfi(eta_hat_mf, false); mfi.isValid(); ++mfi)
+        {
+            const amrex::Box &bx = mfi.tilebox();
+            amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & eta_hat = eta_hat_mf.array(mfi);
+            amrex::Array4<amrex::GpuComplex<Set::Scalar>> const & flux_hat = flux_hat_mf.array(mfi);
 
-            eta_hat(m,n,p) *= scaling;
-        });
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE(int m, int n, int p) {
+                AMREX_D_TERM(
+                    Set::Scalar k1 = m * pi_Lx;,
+                    Set::Scalar k2 = (n < domain.length(1)/2 ? n * pi_Ly : (n - domain.length(1)) * pi_Ly);,
+                    Set::Scalar k3 = (p < domain.length(2)/2 ? p * pi_Lz : (p - domain.length(2)) * pi_Lz););
+
+                Set::Scalar omega2 = AMREX_D_TERM(k1 * k1, + k2 * k2, + k3*k3);
+                Set::Scalar omega4 = omega2 * omega2;
+                amrex::GpuComplex<Set::Scalar> div_flux_hat = AMREX_D_TERM(
+                    I * k1 * flux_hat(m,n,p,0),
+                    + I * k2 * flux_hat(m,n,p,1),
+                    + I * k3 * flux_hat(m,n,p,2));
+
+                eta_hat(m,n,p) = eta_hat(m,n,p) + dt * div_flux_hat + dt * S * omega4 * eta_hat(m,n,p);
+                eta_hat(m,n,p) /= 1.0 + dt * S * omega4;
+                eta_hat(m,n,p) *= scaling;
+            });
+        }
+
+        my_fft.backward(eta_hat_mf, *etanew_mf[lev]);
     }
+    else
+        Util::Abort(INFO,"Invalid mobility: ",mobility);
 
-    //
-    // Transform solution back to realspace
-    //
-    my_fft.backward(eta_hat_mf, *etanew_mf[lev]);
+    etanew_mf[lev]->FillBoundary(geom[lev].periodicity());
 }
 #else
 void
