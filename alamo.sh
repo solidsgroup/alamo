ca2d4bd231d6a62546323e2aaf19d90abbcd08bf
Caleb Munger <mungerct@iastate.edu>
2026-06-25 12:06:29 -0500

    working on increasing lagrange multipler

--- alamo.sh
+++ alamo.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-#SBATCH --time=48:00:00
+#SBATCH --time=12:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --mem=64G
-#SBATCH --job-name="flow_airfoil_supersonic_restart"
+#SBATCH --job-name="flow_airfoil_supersonic_8420_lag"
 #SBATCH --output="%x-%j-log.txt"
 #SBATCH --mail-user=mungerct@iastate.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --constraint=nova25
 
-JOB_NAME="flow_airfoil_supersonic_restart"
+JOB_NAME="flow_airfoil_supersonic_8420_lag"
 PLOT_FILE="output.${JOB_NAME}"
 
 echo "======================================================"
@@ -22,7 +22,8 @@ echo "======================================================"
 
 module load openmpi_hpc
 
-srun --mpi=pmix ./bin/sfi-2d-g++ input.flow_airfoil_supersonic restart=./output.flow_airfoil_supersonic/27580cell restart_cell=./output.flow_airfoil_supersonic/27580cell plot_file="${PLOT_FILE}"
+srun --mpi=pmix ./bin/sfi-2d-g++ input.flow_airfoil_subsonic plot_file="${PLOT_FILE}"
+# srun --mpi=pmix ./bin/sfi-2d-g++ input.flow_airfoil_supersonic restart=./output.flow_airfoil_supersonic/27580cell restart_cell=./output.flow_airfoil_supersonic/27580cell plot_file="${PLOT_FILE}"
 
 echo "======================================================"
 echo " Job finished at: $(date)"
