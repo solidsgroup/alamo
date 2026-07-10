ebda38a0a842e26340a2cf035576ec563746e832
Caleb Munger <mungerct@iastate.edu>
2026-07-10 10:18:57 -0500

    make working 2d test and fix negative sign in hydro bug

--- src/Integrator/Hydro.cpp
+++ src/Integrator/Hydro.cpp
@@ -595,8 +595,7 @@ void Hydro::RHS(int lev, Set::Scalar /*time*/,
                 #endif
             }
 
-
-            Set::Scalar mdot0 = -m0(i,j,k)*grad_eta_mag;
+            Set::Scalar mdot0 = m0(i,j,k)*grad_eta_mag;
             Set::Vector Pdot0 = Set::Vector::Zero(); // Linear momentum source term
             Set::Scalar qdot0 = q0.dot(grad_eta);
 
