9bfcba2b8d4df433b87659cd4ca145486fc0f6a2
Caleb Munger <mungerct@iastate.edu>
2026-07-12 10:21:27 -0500

    Reapply "upadate input file"
    This reverts commit 0fc6703556cd7ce01246f1cd78074837b6155ec9.

--- src/Integrator/Hydro.cpp
+++ src/Integrator/Hydro.cpp
@@ -634,10 +634,6 @@ void Hydro::RHS(int lev, Set::Scalar /*time*/,
                 #endif
             }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e9e9e801d
             Set::Scalar mdot0 = m0(i,j,k)*grad_eta_mag;
             Set::Vector Pdot0 = Set::Vector::Zero(); // Linear momentum source term
             Set::Scalar qdot0 = q0.dot(grad_eta);
