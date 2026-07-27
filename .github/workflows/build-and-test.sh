2a560c0eef162258744b1feb67e8e7394ddaff0f
Brandon Runnels <brunnels@iastate.edu>
2026-07-26 21:24:12 -0500

    add revised GitHub workflows

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -3,7 +3,13 @@ set -eu -o pipefail
 #
 # In the alamo directory, run this command with any additional arguments. 
 #
-./configure 
+compiler=clang++
+for arg in "$@"; do
+	case "$arg" in
+		--comp=*) compiler="${arg#--comp=}" ;;
+	esac
+done
+./configure "$@"
 
 #
 # Compile the code by running make
@@ -18,4 +24,4 @@ ls ./bin/
 #
 # Run the unit test suite in serial using the regression test script
 #
-scripts/runtests.py --dim=3 --serial tests/Unit
+scripts/runtests.py --dim=3 --serial --comp="$compiler" tests/Unit
